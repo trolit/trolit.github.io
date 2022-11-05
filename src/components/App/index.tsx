46b507f0f6395cbebc3edc90d1be37c5b85c4fd9
pawel idzikowski <iddziku@gmail.com>
2022-11-05 01:29:31 +0100

    Update index.tsx

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,7 +3,7 @@ import { MantineProvider } from '@mantine/core';
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-
+      T.B.D
     </MantineProvider>
   );
 }
