42e9287754f7afde8e11c080a89ca1eea7b486c3
pawel idzikowski <iddziku@gmail.com>
2022-11-08 22:12:23 +0100

    Update index.tsx

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,23 @@
-import { MantineProvider } from '@mantine/core';
+import { Container, MantineProvider, BackgroundImage, Center, Box  } from '@mantine/core';
+import { LeadGrid } from './Grid';
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      T.B.D
+      
+      <BackgroundImage
+            src={require("@/assets/images/background.svg").default}
+            radius="xs"
+            style={{height: '100vh'}}
+          >
+            <Center style={{width: '100%', height: '100%', border: '1px solid red'}}>
+            <Container>
+              <LeadGrid />
+              </Container>
+            </Center>
+          </BackgroundImage>
+      
+      
     </MantineProvider>
   );
 }
