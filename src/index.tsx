3c2b2527b81f4a9f40a588f10b20f455aff7a4e7
pawel idzikowski <iddziku@gmail.com>
2022-11-10 17:38:42 +0100

    Revert index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import '@/assets/styles/main.css';
+import './index.css';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(
