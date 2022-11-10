93369e5cdfdbc1ddaa7115278b51934084747afa
pawel idzikowski <iddziku@gmail.com>
2022-11-10 21:23:09 +0100

    Update profile.tsx

--- src/assets/data/profile.tsx
+++ src/assets/data/profile.tsx
@@ -1,4 +1,6 @@
+import { ILink } from '@/interfaces/ILink';
 import { IBadge } from '@/interfaces/IBadge';
+import { IconBrandGithub } from '@tabler/icons';
 
 export const name = 'p4w31 !d21k0w541';
 
@@ -6,4 +8,11 @@ export const badges: IBadge[] = [
   { text: 'TypeScript' },
   { text: 'JavaScript', color: 'yellow' },
   { text: 'C#', color: 'violet' },
+];
+
+export const links: ILink[] = [
+  {
+    icon: IconBrandGithub,
+    url: 'https://github.com/trolit/',
+  },
 ];
