f5c0a84e54642cf361105e6d02fce72d64677628
pawel idzikowski <iddziku@gmail.com>
2022-11-10 20:26:57 +0100

    Update Grid.tsx

--- src/components/App/Grid.tsx
+++ src/components/App/Grid.tsx
@@ -1,14 +1,15 @@
 import { Grid } from '@mantine/core';
 import styles from './app.module.css';
+import { Sidebar } from '@/components/Sidebar';
 
 export function LeadGrid() {
   return (
     <Grid grow className={styles.grid}>
-      <Grid.Col span={12} sm={6}>
-        1
+      <Grid.Col span={4} className={styles.sidebar}>
+        <Sidebar />
       </Grid.Col>
 
-      <Grid.Col span={6}>
+      <Grid.Col span={8}>
         <Grid>
           <Grid.Col span={12}>2</Grid.Col>
 
