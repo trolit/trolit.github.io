170afc03b8bd2ee1d3a2c69154ca4d83462c03c8
pawel idzikowski <iddziku@gmail.com>
2022-11-12 18:46:15 +0100

    Move `avatarSrc` to `profile`

--- src/assets/data/common.tsx
+++ src/assets/data/common.tsx
@@ -1,5 +1,3 @@
-export const avatarSrc = require('@/assets/media/avatar.png');
-
 export const backgroundSrc = require('@/assets/media/background.svg').default;
 
 export const primaryColor = 'violet';
--- src/assets/data/profile.tsx
+++ src/assets/data/profile.tsx
@@ -2,6 +2,8 @@ import { ILink } from '@/interfaces/ILink';
 import { IBadge } from '@/interfaces/IBadge';
 import { IconBrandGithub } from '@tabler/icons';
 
+export const avatarSrc = require('@/assets/media/avatar.png');
+
 export const name = 'p4w31 !d21k0w541';
 
 export const title = 'Software Developer';
