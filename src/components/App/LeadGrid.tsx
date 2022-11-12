ceb0e8c475a4f236fb6bcd6b7cfaf25422849834
pawel idzikowski <iddziku@gmail.com>
2022-11-12 19:23:16 +0100

    Add `Dashboard` to `LeadGrid`

--- src/components/App/LeadGrid.tsx
+++ src/components/App/LeadGrid.tsx
@@ -1,7 +1,8 @@
-import { Grid, Paper } from '@mantine/core';
+import { Grid } from '@mantine/core';
 
 import styles from './styles.module.css';
 import { Profile } from '@/components/Profile';
+import { Dashboard } from '@/components/Dashboard';
 
 export function LeadGrid() {
   return (
@@ -11,20 +12,7 @@ export function LeadGrid() {
       </Grid.Col>
 
       <Grid.Col span={9}>
-        <Paper
-          p='lg'
-          radius='md'
-          withBorder
-          style={{ height: '100%', opacity: 0.8 }}
-        >
-          <Grid>
-            <Grid.Col span={12}>1</Grid.Col>
-
-            <Grid.Col span={3}>2</Grid.Col>
-
-            <Grid.Col span={9}>3</Grid.Col>
-          </Grid>
-        </Paper>
+        <Dashboard />
       </Grid.Col>
     </Grid>
   );
