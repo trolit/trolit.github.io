d7f395eb64a7e5248156b020af9b07ae62966766
pawel idzikowski <iddziku@gmail.com>
2022-11-12 21:37:18 +0100

    Update MainGrid.tsx

--- src/components/App/MainGrid.tsx
+++ src/components/App/MainGrid.tsx
@@ -1,10 +1,26 @@
 import { Grid } from '@mantine/core';
+import { useSelector } from 'react-redux';
 
+import { RootState } from '@/store';
 import styles from './styles.module.css';
 import { Profile } from '@/components/Profile';
 import { Dashboard } from '@/components/Dashboard';
 
 export function MainGrid() {
+  const isDashboardMaximized = useSelector(
+    (state: RootState) => state.preferences.isDashboardMaximized,
+  );
+
+  if (isDashboardMaximized) {
+    return (
+      <Grid grow className={styles.mainGrid}>
+        <Grid.Col span={12}>
+          <Dashboard />
+        </Grid.Col>
+      </Grid>
+    );
+  }
+
   return (
     <Grid grow className={styles.mainGrid}>
       <Grid.Col span={3}>
