a7d423b100d0d9ea575129929c3671ed34042bfb
pawel idzikowski <iddziku@gmail.com>
2022-11-12 18:08:08 +0100

    Update LeadGrid.tsx

--- src/components/App/LeadGrid.tsx
+++ src/components/App/LeadGrid.tsx
@@ -6,11 +6,11 @@ import { Sidebar } from '@/components/Sidebar';
 export function LeadGrid() {
   return (
     <Grid grow className={styles.grid}>
-      <Grid.Col span={4}>
+      <Grid.Col span={3}>
         <Sidebar />
       </Grid.Col>
 
-      <Grid.Col span={8}>
+      <Grid.Col span={9}>
         <Grid>
           <Grid.Col span={12}>2</Grid.Col>
 
