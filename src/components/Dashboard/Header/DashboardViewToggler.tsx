5ee1de550acb7588f93e25d774f09d6c4c036561
pawel idzikowski <iddziku@gmail.com>
2022-11-14 13:04:38 +0100

    hide `DashboardViewToggler` on mobile

--- src/components/Dashboard/Header/DashboardViewToggler.tsx
+++ src/components/Dashboard/Header/DashboardViewToggler.tsx
@@ -3,6 +3,7 @@ import { ActionIcon } from '@mantine/core';
 import { TablerIcon } from '@tabler/icons';
 
 import { primaryColor } from '@/assets/data/common';
+import { useCommonStyles } from '@/assets/styles/common';
 import { toggleDashboardView } from '@/store/preferences';
 
 interface IProps {
@@ -10,14 +11,17 @@ interface IProps {
 }
 
 export function DashboardViewToggler({ icon }: IProps) {
+  const commonStyles = useCommonStyles();
+
   const dispatch = useDispatch();
 
   const Icon = icon;
 
   return (
     <ActionIcon
-      color={primaryColor}
       variant='light'
+      color={primaryColor}
+      className={commonStyles.hiddenMobile}
       onClick={() => dispatch(toggleDashboardView())}
     >
       <Icon size={18} />
