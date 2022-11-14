9bb76a8bded763fd7b0a78399b3a9855a979cbf7
pawel idzikowski <iddziku@gmail.com>
2022-11-14 14:42:21 +0100

    Fix `Toolbar` distortion

--- src/components/Dashboard/Header/Toolbar.tsx
+++ src/components/Dashboard/Header/Toolbar.tsx
@@ -1,14 +1,17 @@
 import { Group } from '@mantine/core';
+import { motion } from 'framer-motion';
 
 import { DashboardViewToggler } from './DashboardViewToggler';
 import { ThemeSwitcher } from '@/components/common/ThemeSwitcher';
 
 export function Toolbar() {
   return (
-    <Group>
-      <ThemeSwitcher />
+    <motion.div layout>
+      <Group>
+        <ThemeSwitcher />
 
-      <DashboardViewToggler />
-    </Group>
+        <DashboardViewToggler />
+      </Group>
+    </motion.div>
   );
 }
