146b5083b04759f0997b05feff3462ac0ae21726
pawel idzikowski <iddziku@gmail.com>
2022-11-14 13:12:05 +0100

    Modify `DashboardViewToggler` component

--- src/components/Dashboard/Header/DashboardViewToggler.tsx
+++ src/components/Dashboard/Header/DashboardViewToggler.tsx
@@ -1,30 +1,35 @@
-import { useDispatch } from 'react-redux';
-import { ActionIcon } from '@mantine/core';
-import { TablerIcon } from '@tabler/icons';
+import { ActionIcon, Tooltip } from '@mantine/core';
+import { useDispatch, useSelector } from 'react-redux';
+import { IconMinimize, IconMaximize } from '@tabler/icons';
 
+import { RootState } from '@/store';
 import { primaryColor } from '@/assets/data/common';
 import { useCommonStyles } from '@/assets/styles/common';
 import { toggleDashboardView } from '@/store/preferences';
 
-interface IProps {
-  icon: TablerIcon;
-}
-
-export function DashboardViewToggler({ icon }: IProps) {
+export function DashboardViewToggler() {
   const commonStyles = useCommonStyles();
 
   const dispatch = useDispatch();
 
-  const Icon = icon;
+  const isDashboardMaximized = useSelector(
+    (state: RootState) => state.preferences.isDashboardMaximized,
+  );
+
+  const label = isDashboardMaximized ? 'Minimize' : 'Maximize';
+
+  const Icon = isDashboardMaximized ? IconMinimize : IconMaximize;
 
   return (
-    <ActionIcon
-      variant='light'
-      color={primaryColor}
-      className={commonStyles.hiddenMobile}
-      onClick={() => dispatch(toggleDashboardView())}
-    >
-      <Icon size={18} />
-    </ActionIcon>
+    <Tooltip label={label} position='bottom' withArrow>
+      <ActionIcon
+        variant='light'
+        color={primaryColor}
+        className={commonStyles.hiddenMobile}
+        onClick={() => dispatch(toggleDashboardView())}
+      >
+        <Icon size={18} />
+      </ActionIcon>
+    </Tooltip>
   );
 }
--- src/components/Dashboard/Header/Toolbar.tsx
+++ src/components/Dashboard/Header/Toolbar.tsx
@@ -1,23 +1,14 @@
-import { RootState } from '@/store';
 import { Group } from '@mantine/core';
-import { useSelector } from 'react-redux';
-import { IconMinimize, IconMaximize } from '@tabler/icons';
 
 import { DashboardViewToggler } from './DashboardViewToggler';
 import { ThemeSwitcher } from '@/components/common/ThemeSwitcher';
 
 export function Toolbar() {
-  const isDashboardMaximized = useSelector(
-    (state: RootState) => state.preferences.isDashboardMaximized,
-  );
-
   return (
     <Group>
       <ThemeSwitcher />
 
-      <DashboardViewToggler
-        icon={isDashboardMaximized ? IconMinimize : IconMaximize}
-      />
+      <DashboardViewToggler />
     </Group>
   );
 }
