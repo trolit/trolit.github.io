27606a3b275df7ac27cdd86e7fc25514e7404307
pawel idzikowski <iddziku@gmail.com>
2022-11-16 16:20:12 +0100

    Add `defaultImageSrc`

--- src/assets/data/common.tsx
+++ src/assets/data/common.tsx
@@ -1,5 +1,7 @@
 export const backgroundSrc = require('@/assets/media/background.svg').default;
 
+export const defaultImageSrc = require('@/assets/media/projects/default.jpg');
+
 export const primaryColor = 'violet';
 
 export const headerHeight = 70;
