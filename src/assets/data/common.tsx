324b64579d961ded9f6c734ef485cf3bce07b016
pawel idzikowski <iddziku@gmail.com>
2022-11-17 19:57:21 +0100

    Update common.tsx

--- src/assets/data/common.tsx
+++ src/assets/data/common.tsx
@@ -2,6 +2,6 @@ export const backgroundSrc = require('@/assets/media/background.svg').default;
 
 export const defaultImageSrc = require('@/assets/media/projects/default.jpg');
 
-export const primaryColor = 'violet';
+export const primaryColor = 'green';
 
-export const headerHeight = 70;
+export const headerHeight = 50;
