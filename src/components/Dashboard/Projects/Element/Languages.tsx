440b8325f8faf05b00df5d903941db89c7cc9cb6
pawel idzikowski <iddziku@gmail.com>
2022-11-18 15:51:39 +0100

    Create Languages.tsx

---
+++ src/components/Dashboard/Projects/Element/Languages.tsx
@@ -0,0 +1,28 @@
+import { Badge, Group } from '@mantine/core';
+
+import { ILanguage } from '@/interfaces/ILanguage';
+
+interface IProps {
+  languages: ILanguage[];
+}
+
+export function Languages({ languages }: IProps) {
+  return (
+    <Group spacing={5} noWrap>
+      {languages.map((language, index) => (
+        <Badge
+          key={`badge-${language.name}-${index}`}
+          variant='filled'
+          radius={index === 0 ? 'md' : 0}
+          style={{
+            borderTopRightRadius: 0,
+            borderBottomLeftRadius: 0,
+            backgroundColor: language.badgeColorInHex,
+          }}
+        >
+          {language.acronym}
+        </Badge>
+      ))}
+    </Group>
+  );
+}
