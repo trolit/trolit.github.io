b370c3f7522fb7fb830828132c0d9f8ab616558b
pawel idzikowski <iddziku@gmail.com>
2022-11-18 15:05:00 +0100

    Update common.tsx

--- src/assets/data/common.tsx
+++ src/assets/data/common.tsx
@@ -1,3 +1,5 @@
+import { ILanguage } from '@/interfaces/ILanguage';
+
 export const backgroundSrc = require('@/assets/media/background.svg').default;
 
 export const defaultImageSrc = require('@/assets/media/projects/default.jpg');
@@ -5,3 +7,23 @@ export const defaultImageSrc = require('@/assets/media/projects/default.jpg');
 export const primaryColor = 'green';
 
 export const headerHeight = 50;
+
+export const languages: Record<string, ILanguage> = {
+  js: {
+    name: 'JavaScript',
+    acronym: 'JS',
+    badgeColorInHex: '#f0db4f',
+  },
+
+  ts: {
+    name: 'TypeScript',
+    acronym: 'TS',
+    badgeColorInHex: '#007acc',
+  },
+
+  csharp: {
+    name: 'C#',
+    acronym: 'C#',
+    badgeColorInHex: '#9b4993',
+  },
+};
