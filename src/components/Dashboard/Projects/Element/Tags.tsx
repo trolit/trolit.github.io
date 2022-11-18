868d1a557fd706c3267c4532e8ccaba63591c37e
pawel idzikowski <iddziku@gmail.com>
2022-11-18 15:59:36 +0100

    Create Tags.tsx

---
+++ src/components/Dashboard/Projects/Element/Tags.tsx
@@ -0,0 +1,19 @@
+import { Group, Badge } from '@mantine/core';
+
+import { ITag } from '@/interfaces/dashboard/ITag';
+
+interface IProps {
+  tags: ITag[];
+}
+
+export function Tags({ tags }: IProps) {
+  return (
+    <Group spacing='sm'>
+      {tags.map(({ text, color }, index) => (
+        <Badge variant='outline' key={`badge-${text}-${index}`} size='sm'>
+          {text}
+        </Badge>
+      ))}
+    </Group>
+  );
+}
