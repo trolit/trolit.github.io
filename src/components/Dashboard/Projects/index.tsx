c5131c5f338bdad68936c9d925a50de4a2de0611
pawel idzikowski <iddziku@gmail.com>
2022-11-18 16:28:13 +0100

    Update index.tsx

--- src/components/Dashboard/Projects/index.tsx
+++ src/components/Dashboard/Projects/index.tsx
@@ -1,3 +1,16 @@
+import { Grid } from '@mantine/core';
+
+import { Element } from './Element';
+import { projects } from '@/assets/data/projects';
+
 export function Projects() {
-  return <div>Projects</div>;
+  return (
+    <Grid gutter='xl' justify='center'>
+      {projects.map((project, index) => (
+        <Grid.Col key={index} xs={12} lg={4}>
+          <Element item={project} />
+        </Grid.Col>
+      ))}
+    </Grid>
+  );
 }
