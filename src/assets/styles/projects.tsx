09a7c28513f34fb358a0228359edb1de551430c6
pawel idzikowski <iddziku@gmail.com>
2022-11-20 12:49:23 +0100

    Create projects.tsx

---
+++ src/assets/styles/projects.tsx
@@ -0,0 +1,31 @@
+import { createStyles } from '@mantine/core';
+
+import { DARK_THEME } from '../constants/themes';
+
+export const useProjectsStyles = () => {
+  const { classes } = createStyles((theme) => {
+    const classes = {
+      overlay: {
+        backgroundColor:
+          theme.colorScheme === DARK_THEME
+            ? theme.colors.dark[6]
+            : theme.colors.gray[1],
+      },
+
+      item: {
+        borderRadius: theme.radius.md,
+        backgroundColor:
+          theme.colorScheme === DARK_THEME ? theme.colors.dark[7] : theme.white,
+
+        '&:hover': {
+          boxShadow: `${theme.shadows.md} !important`,
+          transform: 'scale(1.05)',
+        },
+      },
+    };
+
+    return classes;
+  })();
+
+  return classes;
+};
