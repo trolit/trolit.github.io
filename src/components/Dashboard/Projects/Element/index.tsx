280c8e4b0ee0a4a7f91e4e20b936541cc71e8557
pawel idzikowski <iddziku@gmail.com>
2022-11-20 12:58:57 +0100

    Update index.tsx

--- src/components/Dashboard/Projects/Element/index.tsx
+++ src/components/Dashboard/Projects/Element/index.tsx
@@ -1,7 +1,9 @@
-import { Paper, Grid, Text, Stack, Button } from '@mantine/core';
+import { useState } from 'react';
+import { Paper, Grid, Text, Stack, Button, Box } from '@mantine/core';
 
 import { Tags } from './Tags';
 import { Languages } from './Languages';
+import { LinksOverlay } from './LinksOverlay';
 import { useCommonStyles } from '@/assets/styles/common';
 import { IProject } from '@/interfaces/dashboard/IProject';
 import { Thumbnail } from '@/components/Dashboard/common/Thumbnail';
@@ -11,41 +13,54 @@ interface IProps {
 }
 
 export function Element({ item }: IProps) {
+  const { name, thumbnail, date, description, languages, tags, links } = item;
+
   const commonStyles = useCommonStyles();
+
+  const [isOverlayOpened, toggleOverlay] = useState(false);
 
   return (
     <Paper radius='md' withBorder className={commonStyles.h100}>
-      <Languages languages={item.languages} />
+      <Box className={commonStyles.h100} style={{ position: 'relative' }}>
+        {isOverlayOpened && (
+          <LinksOverlay links={links} toggleOverlay={toggleOverlay} />
+        )}
 
-      <Grid p='lg' className={commonStyles.h100}>
-        <Grid.Col span={12}>
-          <Stack align='center' className={commonStyles.h100}>
-            <Thumbnail name={item.name} value={item.thumbnail} />
+        <Languages languages={languages} />
 
-            <Text>{item.name}</Text>
+        <Grid p='lg' className={commonStyles.h100}>
+          <Grid.Col span={12}>
+            <Stack align='center' className={commonStyles.h100}>
+              <Thumbnail name={name} value={thumbnail} />
 
-            <Text size='sm' c='dimmed'>
-              {item.date}
-            </Text>
+              <Text>{name}</Text>
 
-            <Tags tags={item.tags} />
+              <Text size='sm' c='dimmed'>
+                {date}
+              </Text>
 
-            <Text fz='xs' style={{ flexGrow: '1' }}>
-              {item.description}
-            </Text>
+              <Tags tags={tags} />
 
-            <Button
-              size='sm'
-              radius='xs'
-              color='gray'
-              variant='outline'
-              style={{ alignSelf: 'flex-end' }}
-            >
-              more
-            </Button>
-          </Stack>
-        </Grid.Col>
-      </Grid>
+              <Text fz='xs' style={{ flexGrow: '1' }}>
+                {description}
+              </Text>
+
+              {links.length && (
+                <Button
+                  size='sm'
+                  radius='xs'
+                  color='gray'
+                  variant='outline'
+                  style={{ alignSelf: 'flex-end' }}
+                  onClick={() => toggleOverlay((isVisible) => !isVisible)}
+                >
+                  show more ({links.length})
+                </Button>
+              )}
+            </Stack>
+          </Grid.Col>
+        </Grid>
+      </Box>
     </Paper>
   );
 }
