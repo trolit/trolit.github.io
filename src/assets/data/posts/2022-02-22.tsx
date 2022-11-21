95e694977820d8051fdd3f0e9131904e48b90ea6
pawel idzikowski <iddziku@gmail.com>
2022-11-21 19:43:12 +0100

    init some kind of "posts" data

---
+++ src/assets/data/posts/2022-02-22.tsx
@@ -0,0 +1,16 @@
+import { IPost } from '@/interfaces/dashboard/posts/IPost';
+import { ITitle } from '@/interfaces/dashboard/posts/IHeader';
+
+const title: ITitle = {
+  value: 'This is title1',
+
+  component: 'h1',
+};
+
+export const post: IPost = {
+  name: '',
+
+  date: '2022-02-22',
+
+  components: [title],
+};
