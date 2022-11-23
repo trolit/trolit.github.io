0c409da3e6b89f5e9e40861f512d200137d36df8
pawel idzikowski <iddziku@gmail.com>
2022-11-23 19:19:37 +0100

    Update renderHeader.tsx

--- src/services/usePostRenderer/renderHeader.tsx
+++ src/services/usePostRenderer/renderHeader.tsx
@@ -1,8 +1,9 @@
+import { ReactNode } from 'react';
 import { Title } from '@mantine/core';
 
 import { IHeader } from '@/interfaces/dashboard/posts/IHeader';
 
-export function renderHeader(index: number, header: IHeader) {
+export function renderHeader(index: number, header: IHeader): ReactNode {
   return (
     <Title key={index} order={header.order}>
       {header.value}
