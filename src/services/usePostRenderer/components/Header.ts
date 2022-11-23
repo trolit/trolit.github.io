da1188a58d0189fb2ba80ce055a066904d7eb1b7
pawel idzikowski <iddziku@gmail.com>
2022-11-23 21:26:54 +0100

    Update Header.ts

--- src/services/usePostRenderer/components/Header.ts
+++ src/services/usePostRenderer/components/Header.ts
@@ -1,6 +1,8 @@
 import { Base } from './Base';
 import { IHeader } from '../interfaces/IHeader';
 
-export class Header extends Base<IHeader> {
+class Header extends Base<IHeader> {
   protected key: string = 'header';
 }
+
+export const header = (data: IHeader) => new Header(data);
