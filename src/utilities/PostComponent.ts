8761183245c0cb4a0de2d6e92a2f010ee0f56414
pawel idzikowski <iddziku@gmail.com>
2022-11-23 15:51:17 +0100

    Create `PostComponent` type

---
+++ src/utilities/PostComponent.ts
@@ -0,0 +1,5 @@
+import { IPhoto } from '@/interfaces/dashboard/posts/IPhoto';
+import { IHeader } from '@/interfaces/dashboard/posts/IHeader';
+import { IParagraph } from '@/interfaces/dashboard/posts/IParagraph';
+
+export type PostComponent = IPhoto | IHeader | IParagraph;
