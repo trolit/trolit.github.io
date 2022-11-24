68500735bb56bb6cc77c9d6d1d7025da04ff6327
pawel idzikowski <iddziku@gmail.com>
2022-11-24 20:42:04 +0100

    Create index.ts

---
+++ src/services/usePostRenderer/interfaces/index.ts
@@ -0,0 +1,7 @@
+import { IPhoto } from './IPhoto';
+import { IHeader } from './IHeader';
+import { IBasePost } from './IBasePost';
+import { IParagraph } from './IParagraph';
+import { IComponentRenderer } from './IComponentRenderer';
+
+export type { IPhoto, IHeader, IBasePost, IParagraph, IComponentRenderer };
