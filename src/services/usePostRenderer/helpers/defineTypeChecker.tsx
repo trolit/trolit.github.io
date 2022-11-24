a7a56de11f0f263cd6505910e392435f0d991fd6
pawel idzikowski <iddziku@gmail.com>
2022-11-24 18:24:00 +0100

    Remove unused helpers

--- src/services/usePostRenderer/helpers/defineTypeChecker.tsx
+++
@@ -1,9 +0,0 @@
-export function defineTypeChecker<T>(data: unknown): data is T {
-  if (!data || typeof data !== 'object') {
-    return false;
-  }
-
-  const keys = Object.keys(data);
-
-  return keys.every((key: string) => key in data);
-}
