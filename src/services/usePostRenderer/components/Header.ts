467ae637fd6cd34976269bb21f4c098dbe45262a
pawel idzikowski <iddziku@gmail.com>
2022-11-24 18:14:26 +0100

    use variables from constants

--- src/services/usePostRenderer/components/Header.ts
+++ src/services/usePostRenderer/components/Header.ts
@@ -1,6 +1,7 @@
 import { Base } from './Base';
 import { IHeader } from '../interfaces/IHeader';
+import { HEADER_COMPONENT_KEY } from '../constants';
 
 export class Header extends Base<IHeader> {
-  protected key: string = 'header';
+  protected key: string = HEADER_COMPONENT_KEY;
 }
--- src/services/usePostRenderer/components/Paragraph.ts
+++ src/services/usePostRenderer/components/Paragraph.ts
@@ -1,6 +1,7 @@
 import { Base } from './Base';
 import { IParagraph } from '../interfaces/IParagraph';
+import { PARAGRAPH_COMPONENT_KEY } from '../constants';
 
 export class Paragraph extends Base<IParagraph> {
-  protected key: string = 'paragraph';
+  protected key: string = PARAGRAPH_COMPONENT_KEY;
 }
