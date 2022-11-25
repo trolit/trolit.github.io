7c0b3703ba3175ff29263911baef3a4091d0106f
pawel idzikowski <iddziku@gmail.com>
2022-11-25 21:18:26 +0100

    Create `references` component renderer

--- src/services/usePostRenderer/renderers/index.ts
+++ src/services/usePostRenderer/renderers/index.ts
@@ -2,5 +2,12 @@ import { renderPhoto } from './renderPhoto';
 import { renderVideo } from './renderVideo';
 import { renderHeader } from './renderHeader';
 import { renderParagraph } from './renderParagraph';
+import { renderReferences } from './renderReferences';
 
-export { renderPhoto, renderVideo, renderHeader, renderParagraph };
+export {
+  renderPhoto,
+  renderVideo,
+  renderHeader,
+  renderParagraph,
+  renderReferences,
+};
