ffa50576020b8c2525f99b31020722ba06638e9e
pawel idzikowski <iddziku@gmail.com>
2022-11-25 19:03:23 +0100

    Add `renderParagraphWithReferences`

--- src/services/usePostRenderer/renderers/renderParagraph.tsx
+++ src/services/usePostRenderer/renderers/renderParagraph.tsx
@@ -1,7 +1,59 @@
 import { ReactNode } from 'react';
-import { Text } from '@mantine/core';
+import { Text, TextProps } from '@mantine/core';
+import reactStringReplace from 'react-string-replace';
 
 import { Paragraph } from '../components';
+import { IReferences } from '../interfaces';
+
+function renderComponent(
+  index: number,
+  value: ReactNode | ReactNode[],
+  overrideComponentProps: TextProps | undefined,
+) {
+  return (
+    <Text key={index} align='justify' {...overrideComponentProps}>
+      {value}
+    </Text>
+  );
+}
+
+export function renderParagraphWithReferences(
+  index: number,
+  paragraph: Paragraph,
+  references: IReferences,
+) {
+  const {
+    data: { value, overrideComponentProps },
+  } = paragraph;
+
+  const { items } = references;
+
+  if (!items?.length || !items.some((item) => item.match)) {
+    return renderComponent(index, value, overrideComponentProps);
+  }
+
+  let replacedValue: ReactNode[] = [];
+
+  for (const { match, src } of items) {
+    replacedValue = reactStringReplace(
+      replacedValue.length ? replacedValue : value,
+      match,
+      (match: string, index: number) => (
+        <a
+          key={`${match}-${index}`}
+          href={src}
+          target='_blank'
+          rel='noreferrer'
+          style={{ textDecoration: 'none' }}
+        >
+          {match}
+        </a>
+      ),
+    ) as ReactNode[];
+  }
+
+  return renderComponent(index, replacedValue, overrideComponentProps);
+}
 
 export function renderParagraph(
   index: number,
@@ -11,9 +63,5 @@ export function renderParagraph(
     data: { value, overrideComponentProps },
   } = paragraph;
 
-  return (
-    <Text key={index} align='justify' {...overrideComponentProps}>
-      {value}
-    </Text>
-  );
+  return renderComponent(index, value, overrideComponentProps);
 }
