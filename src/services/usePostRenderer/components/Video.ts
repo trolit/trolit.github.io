7d2159111c3ed1469f0b8d2b3577637975a934f7
pawel idzikowski <iddziku@gmail.com>
2022-11-25 15:43:03 +0100

    Create Video.ts

---
+++ src/services/usePostRenderer/components/Video.ts
@@ -0,0 +1,7 @@
+import { Base } from './Base';
+import { IVideo } from '../interfaces';
+import { VIDEO_COMPONENT_KEY } from '../constants';
+
+export class Video extends Base<IVideo> {
+  protected key: string = VIDEO_COMPONENT_KEY;
+}
