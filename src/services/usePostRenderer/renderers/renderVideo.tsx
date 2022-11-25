9be21fb367cd467af110ba5a66f18df4e1adb949
pawel idzikowski <iddziku@gmail.com>
2022-11-25 16:02:33 +0100

    Update renderVideo.tsx

--- src/services/usePostRenderer/renderers/renderVideo.tsx
+++ src/services/usePostRenderer/renderers/renderVideo.tsx
@@ -3,25 +3,25 @@ import { CSSProperties, ReactNode } from 'react';
 
 import { Video } from '../components';
 
+const containerStyles: CSSProperties = {
+  position: 'relative',
+  overflow: 'hidden',
+  width: '100%',
+  paddingBottom: '56.25%', // 16:9
+};
+
+const iframeStyles: CSSProperties = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+};
+
 export function renderVideo(index: number, video: Video): ReactNode {
   const {
     data: { title, src },
   } = video;
-
-  const containerStyles: CSSProperties = {
-    position: 'relative',
-    overflow: 'hidden',
-    width: '100%',
-    paddingBottom: '56.25%', // 16:9
-  };
-
-  const iframeStyles: CSSProperties = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-  };
 
   return (
     <Container key={index} fluid style={containerStyles}>
