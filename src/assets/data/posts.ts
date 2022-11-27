6bb7d426c904acdb500511301d62a128ea9b053a
pawel idzikowski <iddziku@gmail.com>
2022-11-27 13:54:47 +0100

    Update posts.ts

--- src/assets/data/posts.ts
+++ src/assets/data/posts.ts
@@ -1,8 +1,10 @@
 import { sortByDate } from '@/helpers/sortByDate';
 import { post as post1 } from './posts/2022-02-22';
 import { post as post2 } from './posts/2020-03-06';
+import { post as post3 } from './posts/2021-12-27';
+
 import { IPost } from '@/interfaces/dashboard/IPost';
 
-const rawPosts: IPost[] = [post1, post2];
+const rawPosts: IPost[] = [post1, post2, post3];
 
 export const posts = sortByDate<IPost>(rawPosts);
