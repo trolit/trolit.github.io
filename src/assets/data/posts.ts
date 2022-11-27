f4a175593d3c51e16b12c4be7b05563964a1e4da
ⓘ p4w31 !d21k0w541 <iddziku@gmail.com>
2022-11-27 15:15:22 +0100

    Feature/add post 2022 01 09 (#46)
    * add images
    
    * Create 2022-01-09.ts
    
    * Update posts.ts
    
    * Update 2022-01-09.ts

--- src/assets/data/posts.ts
+++ src/assets/data/posts.ts
@@ -3,9 +3,10 @@ import { post as post1 } from './posts/2022-02-22';
 import { post as post2 } from './posts/2020-03-06';
 import { post as post3 } from './posts/2021-12-27';
 import { post as post4 } from './posts/2021-12-29';
+import { post as post5 } from './posts/2022-01-09';
 
 import { IPost } from '@/interfaces/dashboard/IPost';
 
-const rawPosts: IPost[] = [post1, post2, post3, post4];
+const rawPosts: IPost[] = [post1, post2, post3, post4, post5];
 
 export const posts = sortByDate<IPost>(rawPosts);
---
+++ src/assets/data/posts/2022-01-09.ts
@@ -0,0 +1,71 @@
+import {
+  Photo,
+  Header,
+  Paragraph,
+} from '@/services/usePostRenderer/components';
+import { IPost } from '@/interfaces/dashboard/IPost';
+
+export const post: IPost = {
+  name: `Awesome browser, photo editing tool`,
+
+  abstract: `If you are looking after different tool to edit images and you are not fan of desktop apps then Pixlr might be worth checking. I'm continously using it since 2012 and it still appears to me as the best photo editing tool. It contains all basic tools and nice amount of effects that can be combined to achieve interesting effects.`,
+
+  tags: [{ text: 'Tool' }],
+
+  date: '2022-01-09',
+
+  references: {
+    items: [
+      {
+        title: 'Pixlr',
+        url: 'https://pixlr.com',
+      },
+    ],
+
+    renderInParagraphs: true,
+
+    renderAtTheEnd: true,
+  },
+
+  components: [
+    new Photo({
+      description: '',
+
+      overrideComponentProps: {
+        src: require('@/assets/media/posts/2022-01-09/1.png'),
+      },
+    }),
+
+    new Header({
+      value: `Awesome browser, photo editing tool`,
+
+      overrideComponentProps: {
+        order: 1,
+      },
+    }),
+
+    new Paragraph({
+      value: `Pixlr is known to me since 2012 and the purpose of the post is unforced, voluntary recommendation. It serves me to remove the background, edit photo parts or create new one from collected clippings. As for the advantages it does not require any software installation and account creation. It allows to load images from URL. Project can be saved in format to restore it's structure. Pixlr contains nice effects, stickers, brush for overwriting specified color, cutting out (including lasso). I won't go into details of it's features since there is too much to describe. There are versions X (Express) and E (Editor) available, depending on whether we need to make quick modification (X) or work on advanced project (E). Personally I'm used to Pixlr Editor because Express GUI is too minimalistic to me. Pixlr's can be used for free, no watermarks. Of course there are ads and limits e.g. limited number of icons, stickers to pick from Pixlr but still it isn't that restricted like some other apps where you are literally forced to buy paid version or you won't finish your work or make anything. It can be extended with optional premium package which isn't invasive and adds more features. Below comparision between X and E GUI.`,
+    }),
+
+    new Photo({
+      description: 'Pixlr E',
+
+      overrideComponentProps: {
+        src: require('@/assets/media/posts/2022-01-09/2.png'),
+      },
+    }),
+
+    new Photo({
+      description: 'Pixlr X',
+
+      overrideComponentProps: {
+        src: require('@/assets/media/posts/2022-01-09/3.png'),
+      },
+    }),
+
+    new Paragraph({
+      value: `Pixlr authors constantly improve their software 💙. Recently it has been extended with animations panel. If you are are looking after convenient graphics editor which: isn't complicated, is available online, does not force to create account, free version isn't stripped down in a way that it's unusable, has a lot of interesting features then Pixlr is good candidate to recommend.`,
+    }),
+  ],
+};
