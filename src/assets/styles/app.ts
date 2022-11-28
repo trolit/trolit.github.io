dcc3890fdcd0fed10f0b0d8018929a86910749d3
pawel idzikowski <iddziku@gmail.com>
2022-11-28 20:25:00 +0100

    Update app.ts

--- src/assets/styles/app.ts
+++ src/assets/styles/app.ts
@@ -9,7 +9,10 @@ export const useAppStyles = () => {
       },
 
       container: {
+        padding: 0,
+        width: '100vw',
         height: '100vh',
+        overflow: 'hidden',
       },
     };
 
