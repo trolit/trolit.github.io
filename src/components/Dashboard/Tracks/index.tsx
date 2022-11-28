33e79bd2e3dd00781b2c9f43cfbdf0798566bb0a
pawel idzikowski <iddziku@gmail.com>
2022-11-28 16:29:11 +0100

    Update index.tsx

--- src/components/Dashboard/Tracks/index.tsx
+++ src/components/Dashboard/Tracks/index.tsx
@@ -1,3 +1,39 @@
+import { Table, Container, ScrollArea } from '@mantine/core';
+
+import { Rows } from './Rows';
+
 export function Tracks() {
-  return <div>Tracks</div>;
+  return (
+    <Container p={0} fluid style={{ overflow: 'hidden' }}>
+      <ScrollArea>
+        <Table
+          striped
+          withBorder
+          fontSize='xs'
+          captionSide='top'
+          withColumnBorders
+        >
+          <thead>
+            <tr>
+              <th></th>
+
+              <th>Title</th>
+
+              <th>Authors</th>
+
+              <th>Added on</th>
+
+              <th>Tags</th>
+
+              <th>YouTube</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <Rows />
+          </tbody>
+        </Table>
+      </ScrollArea>
+    </Container>
+  );
 }
