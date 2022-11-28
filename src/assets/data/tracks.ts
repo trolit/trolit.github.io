90a65542b02571aa75d18c869775fe461d4a5bc6
pawel idzikowski <iddziku@gmail.com>
2022-11-28 23:02:19 +0100

    add some more tracks from old portfolio ;d

--- src/assets/data/tracks.ts
+++ src/assets/data/tracks.ts
@@ -7,6 +7,330 @@ const { SOUNDTRACK } = COMMON_TAGS;
 
 const rawTracks: ITrack[] = [
   {
+    authors: ['Thomas Newman'],
+
+    title: `1917`,
+
+    date: '2021-10-23',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'KzmdfOh6su4',
+  },
+
+  {
+    authors: ['Hans Zimmer'],
+
+    title: `Lost But Won (Rush)`,
+
+    date: '2021-10-23',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'kOYcbod5J0w',
+  },
+
+  {
+    authors: ['Joseph Trapanese', 'Joey Batey'],
+
+    title: `Burn Butcher Burn (Witcher, Season 2)`,
+
+    date: '2021-12-26',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'qSxBVHqA-RU',
+  },
+
+  {
+    authors: ['James Horner'],
+
+    title: `Calling To The Wind (Windtalkers)`,
+
+    date: '2021-11-15',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'nXBdsGbc-Ao',
+  },
+
+  {
+    authors: ['Steve Jablonsky'],
+
+    title: `I Rise, You Fall (Transformers)`,
+
+    date: '2021-11-18',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'YmfR2trnNOY',
+  },
+
+  {
+    authors: ['Emanuele Patetta'],
+
+    title: `Death of a Hero`,
+
+    date: '2021-11-18',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'WIuely_hs9A',
+  },
+
+  {
+    authors: ['Joseph Trapanese'],
+
+    title: `Power and purpose (Witcher, Season 2)`,
+
+    date: '2021-12-26',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'A89Mj_JbXoQ',
+  },
+
+  {
+    authors: ['Joseph Trapanese'],
+
+    title: `Who Did This to You? (Witcher, Season 2)`,
+
+    date: '2021-12-26',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'QTa-078grnE',
+  },
+
+  {
+    authors: ['Hans Zimmer'],
+
+    title: `Leave No Man Behind (Black Hawk Down)`,
+
+    date: '2021-11-12',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'BvrYDaWMeRE',
+  },
+
+  {
+    authors: ['Two Steps From Hell'],
+
+    title: `Victory`,
+
+    date: '2021-11-12',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'cjG5gcPcpsg',
+  },
+
+  {
+    authors: ['Brian Tyler'],
+
+    title: `Call Of Duty MW3 (OST)`,
+
+    date: '2021-11-12',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'BDBXjyPfWyA',
+  },
+
+  {
+    authors: ['Rupert Gregson-Williams'],
+
+    title: `Praying (Hacksaw Ridge)`,
+
+    date: '2021-11-12',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'duPXV-OsfEU',
+  },
+
+  {
+    authors: ['Johan Söderqvist', 'Patrik Andrén'],
+
+    title: `Devastation`,
+
+    date: '2021-11-12',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'DXPAqiqAqyg',
+  },
+
+  {
+    authors: ['Hans Zimmer'],
+
+    title: `The Jablonsky variations on a theme by HZ`,
+
+    date: '2021-11-11',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'lxx7i13CPaE',
+  },
+
+  {
+    authors: ['Rachid Taha'],
+
+    title: `Barra Barra (Black Hawk Down)`,
+
+    date: '2021-11-11',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'VmEV6qjY9Nc',
+  },
+
+  {
+    authors: ['Hans Zimmer', 'Lisa Gerrard', 'Klaus Badelt'],
+
+    title: `Now We Are Free (Gladiator)`,
+
+    date: '2021-11-11',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'Y1UiD2sxoWo',
+  },
+
+  {
+    authors: ['Joseph Trapanese'],
+
+    title: `Pain and Desperation (Witcher, Season 2)`,
+
+    date: '2021-12-26',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'tdMH45dDlbM',
+  },
+
+  {
+    authors: ['Steve Jablonsky'],
+
+    title: `Tessa (Transformers)`,
+
+    date: '2021-11-11',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'aNQhcbOIZdA',
+  },
+
+  {
+    authors: ['Hans Zimmer'],
+
+    title: `Rangers Lead The Way`,
+
+    date: '2021-11-04',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'Rp26BJVy0nI',
+  },
+
+  {
+    authors: ['Inon Zur'],
+
+    title: `Dragon Age Origins - Main Theme`,
+
+    date: '2022-11-04',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'oWFEVbfCcOY',
+  },
+
+  {
+    authors: ['Neal Acree', 'Jason Hayes'],
+
+    title: `Arthas My Son`,
+
+    date: '2022-11-04',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: '3m51oKG3BKA',
+  },
+
+  {
+    authors: ['Nick Glennie-Smith', 'Harry Gregson-Williams', 'Hans Zimmer'],
+
+    title: `Hummell Gets The Rockets`,
+
+    date: '2022-08-06',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'wppq0da1Bjc',
+  },
+
+  {
+    authors: ['Ramin Djawadi'],
+
+    title: `Light of the Seven`,
+
+    date: '2022-08-02',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: '6LASz6HAL7E',
+  },
+
+  {
+    authors: ['Mark Petrie', 'Andrew Prahlow'],
+
+    title: `Aeons (with Jon Snow tribute)`,
+
+    date: '2022-08-02',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 't_jwAtCQ4-4',
+  },
+
+  {
+    authors: ['J.T. Peterson'],
+
+    title: `Vers L'avant`,
+
+    date: '2022-08-02',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'hWxpuvFS2a4',
+  },
+
+  {
+    authors: ['Serj Tankian', 'Ramin Djawadi'],
+
+    title: 'The Rains of Castamere (Game Of Thrones)',
+
+    date: '2022-08-02',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'gfYzrXmFN84',
+  },
+
+  {
+    authors: ['Joseph Trapanese'],
+
+    title: 'You Belong With Us (Witcher, Season 2)',
+
+    date: '2021-12-26',
+
+    tags: [SOUNDTRACK],
+
+    youtubeId: 'oS5J9HnBdXU',
+  },
+
+  {
     authors: ['Aliye Mutlu'],
 
     title: 'Dawn of a New Time',
