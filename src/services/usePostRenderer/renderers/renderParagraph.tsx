fe41b8c091308e813601d348bd88e6d95a2b2cf7
pawel idzikowski <iddziku@gmail.com>
2022-11-30 17:33:43 +0100

    Update renderParagraph.tsx

--- src/services/usePostRenderer/renderers/renderParagraph.tsx
+++ src/services/usePostRenderer/renderers/renderParagraph.tsx
@@ -38,12 +38,24 @@ export function renderParagraphWithReferences(
 
   let replacedValue: ReactNode[] = [];
 
+  const getRandomId = () => {
+    return Math.random().toString(6).substring(2, 10);
+  };
+
   for (const { match, url } of items) {
+    if (!match) {
+      continue;
+    }
+
     replacedValue = reactStringReplace(
       replacedValue.length ? replacedValue : value,
       match,
-      (match: string, index: number) => (
-        <Anchor key={`${match}-${index}`} href={url} target='_blank'>
+      (match: string) => (
+        <Anchor
+          key={`paragraph${index}-ref${getRandomId()}`}
+          href={url}
+          target='_blank'
+        >
           {match}
         </Anchor>
       ),
