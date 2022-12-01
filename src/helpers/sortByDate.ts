9734a1f09a5de96afaa08389e2eb6d4bdb023e3e
pawel idzikowski <iddziku@gmail.com>
2022-12-01 18:56:51 +0100

    Update sortByDate.ts

--- src/helpers/sortByDate.ts
+++ src/helpers/sortByDate.ts
@@ -1,18 +1,14 @@
-import dayjs from 'dayjs';
+import { dateSort } from '@/utilities/dateSort';
 
 interface IElement {
   date: string;
 }
 
 export function sortByDate<T>(collection: T[], mode: 'desc' | 'asc' = 'desc') {
-  return [...collection].sort((a, b) => {
-    const { date: firstDate } = a as IElement;
-    const { date: secondDate } = b as IElement;
+  return [...collection].sort((firstItem: T, secondItem: T) => {
+    const { date: first } = firstItem as IElement;
+    const { date: second } = secondItem as IElement;
 
-    if (mode === 'desc') {
-      return dayjs(firstDate).isBefore(dayjs(secondDate)) ? 1 : -1;
-    }
-
-    return dayjs(firstDate).isAfter(dayjs(secondDate)) ? 1 : -1;
+    return dateSort(first, second, mode);
   });
 }
