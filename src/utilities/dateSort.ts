a0553bbbbaeb5fb7f200a66a736a6db3d81f4017
pawel idzikowski <iddziku@gmail.com>
2022-12-01 18:56:27 +0100

    Simplify `dateSort`

--- src/utilities/dateSort.ts
+++ src/utilities/dateSort.ts
@@ -1,11 +1,13 @@
 import dayjs from 'dayjs';
 
-export const dateSort =
-  (mode: 'desc' | 'asc' = 'desc') =>
-  (a: string, b: string) => {
-    if (mode === 'desc') {
-      return dayjs(a).isBefore(dayjs(b)) ? 1 : -1;
-    }
+export const dateSort = (
+  a: string,
+  b: string,
+  mode: 'desc' | 'asc' = 'desc',
+) => {
+  if (mode === 'desc') {
+    return dayjs(a).isBefore(dayjs(b)) ? 1 : -1;
+  }
 
-    return dayjs(a).isAfter(dayjs(b)) ? 1 : -1;
-  };
+  return dayjs(a).isAfter(dayjs(b)) ? 1 : -1;
+};
