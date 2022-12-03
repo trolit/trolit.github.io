0a98f6a5d125396c69455c23cbfbcef1b208c341
pawel idzikowski <iddziku@gmail.com>
2022-12-03 21:45:58 +0100

    Add `navigate` to Home `PostElement`

--- src/components/Dashboard/Home/Element/Post.tsx
+++ src/components/Dashboard/Home/Element/Post.tsx
@@ -1,14 +1,23 @@
-import { Badge } from '@mantine/core';
+import { Badge, Anchor } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
 
 import { Element } from '.';
+import { POSTS } from '@/assets/data/posts';
 import { IPost } from '@/interfaces/dashboard/IPost';
+import { POSTS_ROUTE } from '@/assets/constants/routes';
 import { POSTS_NAVIGATION_ITEM } from '@/assets/constants/navigation-items';
 
 interface IProps {
   item: IPost;
 }
 
-export function PostElement({ item: { name, tags } }: IProps) {
+export function PostElement({ item }: IProps) {
+  const navigate = useNavigate();
+
+  const id = POSTS.indexOf(item) + 1;
+
+  const { name, tags } = item;
+
   const extra = tags.map(({ text, color }, index) => (
     <Badge
       size='xs'
@@ -21,7 +30,18 @@ export function PostElement({ item: { name, tags } }: IProps) {
     </Badge>
   ));
 
+  const postExtra = (
+    <Anchor size='xs' onClick={() => navigate(`${POSTS_ROUTE}/${id}`)}>
+      View
+    </Anchor>
+  );
+
   return (
-    <Element icon={POSTS_NAVIGATION_ITEM.icon} text={name} extra={extra} />
+    <Element
+      icon={POSTS_NAVIGATION_ITEM.icon}
+      text={name}
+      extra={extra}
+      postExtra={postExtra}
+    />
   );
 }
