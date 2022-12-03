02abb912863cdd98386b217d6fcea47a4f5c7886
pawel idzikowski <iddziku@gmail.com>
2022-12-03 19:12:05 +0100

    Update Link.tsx

--- src/components/Dashboard/common/Link.tsx
+++ src/components/Dashboard/common/Link.tsx
@@ -1,29 +1,34 @@
-import { Text, UnstyledButton, Group } from '@mantine/core';
+import { Text, UnstyledButton, Stack } from '@mantine/core';
 
 import { ILink } from '@/interfaces/ILink';
+import { useCommonStyles } from '@/assets/styles/common';
 import { useDashboardStyles } from '@/assets/styles/dashboard';
 
 interface IProps {
   item: ILink;
 }
 
-export function Link({ item }: IProps) {
-  const dashboardStyles = useDashboardStyles();
+export function Link({ item: { icon: Icon, name, url } }: IProps) {
+  const { h100 } = useCommonStyles();
+
+  const { link } = useDashboardStyles();
 
   return (
     <UnstyledButton
-      className={dashboardStyles.subLink}
+      p='md'
+      color='black'
+      className={link}
       onClick={() => {
-        window.open(item.url, '_blank');
+        window.open(url, '_blank');
       }}
     >
-      <Group align='flex-start'>
-        <div>
-          <Text size='sm' weight={500}>
-            {item.name}
-          </Text>
-        </div>
-      </Group>
+      <Stack spacing={0} align='center' justify='center' className={h100}>
+        <Icon size={32} />
+
+        <Text fz='xs' c='dimmed' lineClamp={1} mt={7}>
+          {name}
+        </Text>
+      </Stack>
     </UnstyledButton>
   );
 }
