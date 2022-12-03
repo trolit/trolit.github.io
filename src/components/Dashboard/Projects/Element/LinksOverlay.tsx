274a6b1a09403886cb851dd46c790dfeee2aa54b
pawel idzikowski <iddziku@gmail.com>
2022-12-03 19:12:47 +0100

    Update LinksOverlay.tsx

--- src/components/Dashboard/Projects/Element/LinksOverlay.tsx
+++ src/components/Dashboard/Projects/Element/LinksOverlay.tsx
@@ -1,17 +1,16 @@
 import {
-  Text,
   Group,
   Stack,
   Button,
   Overlay,
   ScrollArea,
   SimpleGrid,
-  UnstyledButton,
 } from '@mantine/core';
 import { Dispatch, SetStateAction } from 'react';
 
 import { ILink } from '@/interfaces/ILink';
 import { useCommonStyles } from '@/assets/styles/common';
+import { Link } from '@/components/Dashboard/common/Link';
 import { useProjectsStyles } from '@/assets/styles/projects';
 
 interface IProps {
@@ -21,42 +20,18 @@ interface IProps {
 }
 
 export function LinksOverlay({ links, toggleOverlay }: IProps) {
-  const commonStyles = useCommonStyles();
+  const { h100 } = useCommonStyles();
 
   const projectsStyles = useProjectsStyles();
 
   return (
     <Overlay radius='md' opacity={1}>
-      <Stack
-        p='md'
-        className={`${commonStyles.h100} ${projectsStyles.overlay}`}
-      >
+      <Stack p='md' className={`${h100} ${projectsStyles.overlay}`}>
         <ScrollArea style={{ flexGrow: 1 }}>
           <Group position='apart' align='start' grow>
             <SimpleGrid p={5} cols={3}>
               {links.map((link, index) => (
-                <UnstyledButton
-                  p='md'
-                  key={index}
-                  color='black'
-                  className={projectsStyles.item}
-                  onClick={() => {
-                    window.open(link.url, '_blank');
-                  }}
-                >
-                  <Stack
-                    spacing={0}
-                    align='center'
-                    justify='center'
-                    className={commonStyles.h100}
-                  >
-                    <link.icon size={32} />
-
-                    <Text fz='xs' c='dimmed' lineClamp={1} mt={7}>
-                      {link.name}
-                    </Text>
-                  </Stack>
-                </UnstyledButton>
+                <Link key={`link-${index}`} item={link} />
               ))}
             </SimpleGrid>
           </Group>
