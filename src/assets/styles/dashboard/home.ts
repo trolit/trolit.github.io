08c0b16791b864e5db28777f1e5fb833aa9f14c1
pawel idzikowski <iddziku@gmail.com>
2022-12-06 17:51:48 +0100

    Update home.ts

--- src/assets/styles/dashboard/home.ts
+++ src/assets/styles/dashboard/home.ts
@@ -1,46 +1,45 @@
 import { createStyles } from '@mantine/core';
 
-import { HEADER_HEIGHT, OVERLAY_PADDING } from '@/config';
+import { HEADER_HEIGHT, OVERLAY_PADDING, DASHBOARD_PADDING } from '@/config';
 
-const TITLE_HEIGHT = 30;
+const TITLE_HEIGHT = '30px';
 
-const INNER_PADDING = 15;
+const INNER_PADDING = '10px';
 
-const SCROLL_PADDING = 12;
+const DATE_BADGE_HEIGHT = '30px';
 
-const DATE_BADGE_HEIGHT = 30;
+const HORIZONTAL_SCROLL_PADDING = '12px';
 
 export const useHomeStyles = () => {
   const { classes } = createStyles((theme) => {
-    const { spacing } = theme;
-
-    const AVAILABLE_HEIGHT = `calc(100vh - ${HEADER_HEIGHT}px - ${OVERLAY_PADDING}px - ${
-      spacing.lg * 2
-    }px - ${SCROLL_PADDING}px)`;
+    const AVAILABLE_HEIGHT = `calc(100vh - ${HEADER_HEIGHT}px - ${
+      DASHBOARD_PADDING * 2
+    }px - ${OVERLAY_PADDING}px)`;
 
     const classes = {
       wrapper: {
         padding: 0,
-        height: `${AVAILABLE_HEIGHT}px`,
+        height: `${AVAILABLE_HEIGHT}`,
       },
 
       title: {
-        height: `${TITLE_HEIGHT}px`,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: `${TITLE_HEIGHT}`,
       },
 
       horizontalScroll: {
-        height: `calc(100% - ${TITLE_HEIGHT}px)`,
+        height: `calc(${AVAILABLE_HEIGHT} - ${TITLE_HEIGHT})`,
       },
 
-      elements: {
-        padding: `${INNER_PADDING}px`,
-        height: `calc(${AVAILABLE_HEIGHT} - ${
-          INNER_PADDING * 2
-        }px - ${DATE_BADGE_HEIGHT}px)`,
+      items: {
+        padding: `0 ${INNER_PADDING} ${INNER_PADDING} ${INNER_PADDING}`,
+        height: `calc(${AVAILABLE_HEIGHT} - ${TITLE_HEIGHT} - ${DATE_BADGE_HEIGHT} - ${HORIZONTAL_SCROLL_PADDING})`,
       },
 
       date: {
-        height: `${DATE_BADGE_HEIGHT}px`,
+        height: `${DATE_BADGE_HEIGHT}`,
       },
     };
 
