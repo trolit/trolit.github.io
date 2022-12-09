91aa93170ed6ac7557261c835093cddbc8cdd8fc
pawel idzikowski <iddziku@gmail.com>
2022-12-09 21:35:50 +0100

    Create home-view-thresholds.ts

---
+++ src/assets/constants/home-view-thresholds.ts
@@ -0,0 +1,59 @@
+import { IHomeViewThreshold } from '@/interfaces/IHomeViewThreshold';
+
+const commonThresholdValue = '861px';
+
+export const HOME_VIEW_THRESHOLDS: IHomeViewThreshold[] = [
+  {
+    point: 1,
+
+    value: commonThresholdValue,
+  },
+
+  {
+    point: 2,
+
+    value: commonThresholdValue,
+  },
+
+  {
+    point: 3,
+
+    value: commonThresholdValue,
+  },
+
+  {
+    point: 4,
+
+    value: commonThresholdValue,
+  },
+
+  {
+    point: 5,
+
+    value: '959px',
+  },
+
+  {
+    point: 6,
+
+    value: '1205px',
+  },
+
+  {
+    point: 7,
+
+    value: '1441px',
+  },
+
+  {
+    point: 8,
+
+    value: '1665px',
+  },
+
+  {
+    point: 9,
+
+    value: '1896px',
+  },
+];
