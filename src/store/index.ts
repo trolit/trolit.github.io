bae97844fdb8cc62ce3bc15f5ce04c2091b38167
pawel idzikowski <iddziku@gmail.com>
2022-12-09 22:05:57 +0100

    Update store config file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import dashboardReducer from './dashboard';
 import preferencesReducer from './preferences';
 
 export const store = configureStore({
   reducer: {
+    dashboard: dashboardReducer,
+
     preferences: preferencesReducer,
   },
 });
