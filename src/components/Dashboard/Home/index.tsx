0a6d7ea3d09ab2c20dff28c38ae48d446ed0dd82
pawel idzikowski <iddziku@gmail.com>
2022-12-09 23:23:34 +0100

    Update index.tsx

--- src/components/Dashboard/Home/index.tsx
+++ src/components/Dashboard/Home/index.tsx
@@ -1,17 +1,12 @@
-import { Title, Container, ScrollArea } from '@mantine/core';
-import { IconClock2 } from '@tabler/icons';
+import { Container, ScrollArea } from '@mantine/core';
 import { Board } from './Board';
 import { useHomeStyles } from '@/assets/styles/dashboard/home';
 
 export function Home() {
-  const { title, horizontalScroll } = useHomeStyles();
+  const { horizontalScroll } = useHomeStyles();
 
   return (
     <Container p={0} fluid>
-      <Title order={4} align='center' className={title}>
-        <IconClock2 /> &nbsp; Recent Activity
-      </Title>
-
       <ScrollArea className={horizontalScroll}>
         <Container p={0} fluid>
           <Board />
