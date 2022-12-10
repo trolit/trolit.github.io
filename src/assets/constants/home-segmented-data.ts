f83958131f25c52de67648000d8c2ee089620cfc
pawel idzikowski <iddziku@gmail.com>
2022-12-11 00:59:20 +0100

    Update home-segmented-data.ts

--- src/assets/constants/home-segmented-data.ts
+++ src/assets/constants/home-segmented-data.ts
@@ -1,47 +1,72 @@
 import dayjs, { Dayjs } from 'dayjs';
 
+import { HomeSegment } from '@/enums/HomeSegment';
+import { IPost } from '@/interfaces/dashboard/IPost';
+import { ITrack } from '@/interfaces/dashboard/ITrack';
+import { IProject } from '@/interfaces/dashboard/IProject';
 import { HOME_GROUP_BY, HOME_INTEREST_POINTS } from '@/config';
 import { getPointsOfInterest } from '@/helpers/getPointsOfInterest';
 import { ALL_DATES as POSTS_DATES, POSTS } from '@/assets/data/posts';
 import { ALL_DATES as TRACKS_DATES, TRACKS } from '@/assets/data/tracks';
 import { ALL_DATES as PROJECTS_DATES, PROJECTS } from '@/assets/data/projects';
 
-export const PROJECTS_POINTS_OF_INTEREST = getPointsOfInterest(
-  PROJECTS_DATES,
-  HOME_INTEREST_POINTS,
-  HOME_GROUP_BY,
-);
+const homeSegments = [
+  {
+    value: HomeSegment.PROJECTS,
+    dates: PROJECTS_DATES,
+    items: PROJECTS,
+  },
 
-export const PROJECTS_ARRAY_OF_INDEXES = getArrayOfIndexes(
-  PROJECTS_POINTS_OF_INTEREST,
-  PROJECTS,
-);
+  {
+    value: HomeSegment.POSTS,
+    dates: POSTS_DATES,
+    items: POSTS,
+  },
 
-export const TRACKS_POINTS_OF_INTEREST = getPointsOfInterest(
-  TRACKS_DATES,
-  HOME_INTEREST_POINTS,
-  HOME_GROUP_BY,
-);
+  {
+    value: HomeSegment.TRACKS,
+    dates: TRACKS_DATES,
+    items: TRACKS,
+  },
+];
 
-export const TRACKS_ARRAY_OF_INDEXES = getArrayOfIndexes(
-  TRACKS_POINTS_OF_INTEREST,
-  TRACKS,
-);
+interface ISegmentData {
+  value: HomeSegment;
 
-export const POSTS_POINTS_OF_INTEREST = getPointsOfInterest(
-  POSTS_DATES,
-  HOME_INTEREST_POINTS,
-  HOME_GROUP_BY,
-);
+  pointsOfInterest: string[];
 
-export const POSTS_ARRAY_OF_INDEXES = getArrayOfIndexes(
-  POSTS_POINTS_OF_INTEREST,
-  POSTS,
-);
+  arrayOfIndexes: number[][];
+}
 
-function getArrayOfIndexes<T>(
+const CONSTRUCTED_SEGMENTS: ISegmentData[] = [];
+
+for (const { value, dates, items } of homeSegments) {
+  const pointsOfInterest = getPointsOfInterest(
+    dates,
+    HOME_INTEREST_POINTS,
+    HOME_GROUP_BY,
+  );
+
+  const arrayOfIndexes = getArrayOfIndexes(pointsOfInterest, items);
+
+  CONSTRUCTED_SEGMENTS.push({
+    value,
+    pointsOfInterest,
+    arrayOfIndexes,
+  });
+}
+
+export function getDataBySegment(segment: HomeSegment) {
+  return (
+    CONSTRUCTED_SEGMENTS.find(
+      (constructedSegment) => constructedSegment.value === segment,
+    ) || CONSTRUCTED_SEGMENTS[0]
+  );
+}
+
+function getArrayOfIndexes(
   pointsOfInterest: string[],
-  source: T[],
+  source: IProject[] | IPost[] | ITrack[],
 ): number[][] {
   const arrayOfIndexes: number[][] = [];
 
@@ -53,7 +78,7 @@ function getArrayOfIndexes<T>(
     (pointOfInterest) => dayjs(pointOfInterest),
   );
 
-  source.forEach((item: T, index: number) => {
+  source.forEach((item: IProject | IPost | ITrack, index: number) => {
     const castedItem = item as { date: string };
 
     const pointOfInterestIndex = parsedPointsOfInterest.findIndex(
