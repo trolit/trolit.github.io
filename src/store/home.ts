2d577fb916c85f7740b05a377daa77cd471de543
pawel idzikowski <iddziku@gmail.com>
2022-12-11 00:59:59 +0100

    Update home.ts

--- src/store/home.ts
+++ src/store/home.ts
@@ -1,35 +1,26 @@
-import dayjs from 'dayjs';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { HomeSegment } from '@/enums/HomeSegment';
-import { IPost } from '@/interfaces/dashboard/IPost';
-import { ITrack } from '@/interfaces/dashboard/ITrack';
-import { IProject } from '@/interfaces/dashboard/IProject';
-import { IBoardData } from '@/interfaces/dashboard/IBoardData';
-import { HOME_GROUP_BY, HOME_INTEREST_POINTS } from '@/config';
-import { getPointsOfInterest } from '@/helpers/getPointsOfInterest';
-import { ALL_DATES as POSTS_DATES, POSTS } from '@/assets/data/posts';
-import { ALL_DATES as TRACKS_DATES, TRACKS } from '@/assets/data/tracks';
-import { ALL_DATES as PROJECTS_DATES, PROJECTS } from '@/assets/data/projects';
+import { getDataBySegment } from '@/assets/constants/home-segmented-data';
 
 interface IState {
   activeSegment: HomeSegment;
 
-  projects: IBoardData<IProject>;
+  pointsOfInterest: string[];
 
-  tracks: IBoardData<ITrack>;
-
-  posts: IBoardData<IPost>;
+  arrayOfIndexes: number[][];
 }
 
+const activeSegment = HomeSegment.PROJECTS;
+
+const { pointsOfInterest, arrayOfIndexes } = getDataBySegment(activeSegment);
+
 const initialState: IState = {
-  activeSegment: HomeSegment.PROJECTS,
+  activeSegment,
 
-  projects: filterData(PROJECTS, PROJECTS_DATES),
+  pointsOfInterest,
 
-  tracks: filterData(TRACKS, TRACKS_DATES),
-
-  posts: filterData(POSTS, POSTS_DATES),
+  arrayOfIndexes,
 };
 
 export const homeSlice = createSlice({
@@ -45,28 +36,6 @@ export const homeSlice = createSlice({
     },
   },
 });
-
-function filterData<T>(source: T[], dates: string[]): IBoardData<T> {
-  const pointsOfInterest = getPointsOfInterest(
-    dates,
-    HOME_INTEREST_POINTS,
-    HOME_GROUP_BY,
-  );
-
-  const arrayOfItems = pointsOfInterest.map((pointOfInterest) =>
-    source.filter((item: T) => {
-      const castedItem = item as { date: string };
-
-      return pointOfInterest.isSame(dayjs(castedItem.date), HOME_GROUP_BY);
-    }),
-  );
-
-  return {
-    pointsOfInterest,
-
-    arrayOfItems,
-  };
-}
 
 export const { setActiveSegment } = homeSlice.actions;
 
