af29e86f4810bb3a72e4c006b668666bac29f584
pawel idzikowski <iddziku@gmail.com>
2022-12-13 20:05:34 +0100

    Update home-view-thresholds.ts

--- src/assets/constants/home-view-thresholds.ts
+++ src/assets/constants/home-view-thresholds.ts
@@ -1,61 +1,63 @@
 import { MAX_HOME_INTEREST_POINTS } from '@/config';
 import { IHomeViewThreshold } from '@/interfaces/IHomeViewThreshold';
 
-const commonThresholdValue = '861px';
+const commonThresholdValue1 = '1249px';
+
+const commonThresholdValue2 = '1892px';
 
 export const HOME_VIEW_THRESHOLDS: IHomeViewThreshold[] = [
   {
     points: 1,
 
-    value: commonThresholdValue,
+    value: commonThresholdValue1,
   },
 
   {
     points: 2,
 
-    value: commonThresholdValue,
+    value: commonThresholdValue1,
   },
 
   {
     points: 3,
 
-    value: commonThresholdValue,
+    value: commonThresholdValue1,
   },
 
   {
     points: 4,
 
-    value: commonThresholdValue,
+    value: commonThresholdValue1,
   },
 
   {
     points: 5,
 
-    value: '959px',
+    value: '1359px',
   },
 
   {
     points: 6,
 
-    value: '1205px',
+    value: '1625px',
   },
 
   {
     points: 7,
 
-    value: '1441px',
+    value: commonThresholdValue2,
   },
 
   {
     points: 8,
 
-    value: '1665px',
+    value: commonThresholdValue2,
   },
 
   {
     points: 9,
 
-    value: '1896px',
+    value: commonThresholdValue2,
   },
 ];
 
