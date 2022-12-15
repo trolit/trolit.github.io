9b0c5c217a5a280f3b856f1a40adbf69bba0411c
pawel idzikowski <iddziku@gmail.com>
2022-12-15 19:50:52 +0100

    Update Subheader.tsx

--- src/components/Dashboard/Projects/Subheader.tsx
+++ src/components/Dashboard/Projects/Subheader.tsx
@@ -1,20 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Paper, SegmentedControl } from '@mantine/core';
 
+import { RootState } from '@/store';
 import { PRIMARY_COLOR } from '@/config';
-import { HomeSegment } from '@/enums/HomeSegment';
+import { setActiveSegment } from '@/store/projects';
+import { ProjectsSegment } from '@/enums/ProjectsSegment';
 import { useProjectsStyles } from '@/assets/styles/dashboard/projects';
 
 export function Subheader() {
   const { subheader } = useProjectsStyles();
 
+  const dispatch = useDispatch();
+
+  const activeSegment = useSelector(
+    (state: RootState) => state.projects.activeSegment,
+  );
+
   return (
     <Paper radius={0} className={subheader}>
       <SegmentedControl
+        value={activeSegment}
         color={PRIMARY_COLOR}
         data={[
-          { label: 'All', value: HomeSegment.PROJECTS },
-          { label: 'Featured', value: HomeSegment.POSTS },
+          { label: 'All', value: ProjectsSegment.ALL },
+          { label: 'Featured', value: ProjectsSegment.FEATURED },
         ]}
+        onChange={(value: ProjectsSegment) => dispatch(setActiveSegment(value))}
       />
     </Paper>
   );
