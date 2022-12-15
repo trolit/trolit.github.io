f9e775256d8da6b057f937530672007ae64a21c9
pawel idzikowski <iddziku@gmail.com>
2022-12-15 20:33:06 +0100

    Update index.tsx

--- src/components/Dashboard/Projects/index.tsx
+++ src/components/Dashboard/Projects/index.tsx
@@ -1,23 +1,38 @@
+import { useSelector } from 'react-redux';
 import { Container, Grid, ScrollArea } from '@mantine/core';
 
 import { Element } from './Element';
+import { RootState } from '@/store';
+import { Featured } from './Featured';
 import { DASHBOARD_PADDING } from '@/config';
 import { PROJECTS } from '@/assets/data/projects';
+import { ProjectsSegment } from '@/enums/ProjectsSegment';
 import { useProjectsStyles } from '@/assets/styles/dashboard/projects';
 
 export function Projects() {
   const { scrollArea } = useProjectsStyles();
 
+  const activeSegment = useSelector(
+    (state: RootState) => state.projects.activeSegment,
+  );
+
+  const content =
+    activeSegment === ProjectsSegment.ALL ? (
+      <Grid gutter='xl' justify='center'>
+        {PROJECTS.map((project, index) => (
+          <Grid.Col key={`project-${index}`} xs={12} lg={4}>
+            <Element item={project} />
+          </Grid.Col>
+        ))}
+      </Grid>
+    ) : (
+      <Featured />
+    );
+
   return (
     <ScrollArea className={scrollArea}>
       <Container pt={0} p={DASHBOARD_PADDING} fluid>
-        <Grid gutter='xl' justify='center'>
-          {PROJECTS.map((project, index) => (
-            <Grid.Col key={`project-${index}`} xs={12} lg={4}>
-              <Element item={project} />
-            </Grid.Col>
-          ))}
-        </Grid>
+        {content}
       </Container>
     </ScrollArea>
   );
