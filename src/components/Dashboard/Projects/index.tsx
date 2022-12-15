b8761f0f0ca6f081e97ebcb91129dfb877dffb63
pawel idzikowski <iddziku@gmail.com>
2022-12-15 20:44:59 +0100

    extract "all projects" to separate dir

--- src/components/Dashboard/Projects/index.tsx
+++ src/components/Dashboard/Projects/index.tsx
@@ -1,11 +1,10 @@
 import { useSelector } from 'react-redux';
-import { Container, Grid, ScrollArea } from '@mantine/core';
+import { Container, ScrollArea } from '@mantine/core';
 
-import { Element } from './Element';
+import { All } from './All';
 import { RootState } from '@/store';
 import { Featured } from './Featured';
 import { DASHBOARD_PADDING } from '@/config';
-import { PROJECTS } from '@/assets/data/projects';
 import { ProjectsSegment } from '@/enums/ProjectsSegment';
 import { useProjectsStyles } from '@/assets/styles/dashboard/projects';
 
@@ -16,23 +15,10 @@ export function Projects() {
     (state: RootState) => state.projects.activeSegment,
   );
 
-  const content =
-    activeSegment === ProjectsSegment.ALL ? (
-      <Grid gutter='xl' justify='center'>
-        {PROJECTS.map((project, index) => (
-          <Grid.Col key={`project-${index}`} xs={12} lg={4}>
-            <Element item={project} />
-          </Grid.Col>
-        ))}
-      </Grid>
-    ) : (
-      <Featured />
-    );
-
   return (
     <ScrollArea className={scrollArea}>
       <Container pt={0} p={DASHBOARD_PADDING} fluid>
-        {content}
+        {activeSegment === ProjectsSegment.ALL ? <All /> : <Featured />}
       </Container>
     </ScrollArea>
   );
