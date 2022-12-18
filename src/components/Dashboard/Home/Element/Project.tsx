74cf0c2830f94ede3f700769c961eae7705465f3
pawel idzikowski <iddziku@gmail.com>
2022-12-18 21:49:51 +0100

    Update Project.tsx

--- src/components/Dashboard/Home/Element/Project.tsx
+++ src/components/Dashboard/Home/Element/Project.tsx
@@ -1,10 +1,11 @@
-import { ActionIcon, Group } from '@mantine/core';
+import { Group } from '@mantine/core';
 
 import { Element } from '.';
 import { Tags } from '@/components/Dashboard/common/Tags';
 import { IProject } from '@/interfaces/dashboard/IProject';
 import { Languages } from '@/components/Dashboard/common/Languages';
 import { PROJECTS_NAVIGATION_ITEM } from '@/assets/constants/navigation-items';
+import { ActionIconLinks } from '@/components/Dashboard/common/ActionIconLinks';
 
 interface IProps {
   item: IProject;
@@ -13,36 +14,33 @@ interface IProps {
 export function ProjectElement({
   item: { name, languages, tags, links },
 }: IProps) {
-  const renderedLinks = (
+  const label = (
+    <Languages
+      name={name}
+      value={languages}
+      badgeProps={{ mr: 5, size: 'sm', radius: 0, variant: 'outline' }}
+    />
+  );
+
+  const extra = <Tags name={name} value={tags} badgeProps={{ mr: 5 }} />;
+
+  const postExtra = (
     <Group mt={5} spacing={5}>
-      {links.map(({ icon: Icon, url }, index) => (
-        <ActionIcon
-          key={`${name}-link-${index}`}
-          variant='subtle'
-          size={22}
-          onClick={() => {
-            window.open(url, '_blank');
-          }}
-        >
-          <Icon />
-        </ActionIcon>
-      ))}
+      <ActionIconLinks
+        name={name}
+        value={links}
+        actionIconProps={{ size: 22, variant: 'subtle' }}
+      />
     </Group>
   );
 
   return (
     <Element
       text={name}
+      extra={extra}
+      label={label}
       textLineClamp={1}
-      extra={<Tags name={name} value={tags} badgeProps={{ mr: 5 }} />}
-      postExtra={renderedLinks}
-      label={
-        <Languages
-          name={name}
-          value={languages}
-          badgeProps={{ mr: 5, size: 'sm', radius: 0, variant: 'outline' }}
-        />
-      }
+      postExtra={postExtra}
       icon={PROJECTS_NAVIGATION_ITEM.icon}
     />
   );
