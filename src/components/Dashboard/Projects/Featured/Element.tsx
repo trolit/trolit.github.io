547794b5a6a4f0fcf008a8d6852b73fbccc1a964
pawel idzikowski <iddziku@gmail.com>
2022-12-18 21:53:16 +0100

    Update Element.tsx

--- src/components/Dashboard/Projects/Featured/Element.tsx
+++ src/components/Dashboard/Projects/Featured/Element.tsx
@@ -9,13 +9,13 @@ import {
   Popover,
   Button,
   Container,
-  ActionIcon,
   ScrollArea,
 } from '@mantine/core';
 
 import { Languages } from '@/components/Dashboard/common/Languages';
 import { IFeaturedProject } from '@/interfaces/dashboard/IFeaturedProject';
 import { FormattedDate } from '@/components/Dashboard/common/FormattedDate';
+import { ActionIconLinks } from '@/components/Dashboard/common/ActionIconLinks';
 import { useFeaturedProjectsStyles } from '@/assets/styles/dashboard/projects/featured';
 
 interface IProps {
@@ -86,19 +86,11 @@ export function Element({
 
           <Card.Section withBorder>
             <Group py={10} noWrap spacing={15} position='center'>
-              {links.map(({ icon: Icon, url }, index) => (
-                <ActionIcon
-                  key={`${name}-link-${index}`}
-                  color='gray'
-                  variant='outline'
-                  size={50}
-                  onClick={() => {
-                    window.open(url, '_blank');
-                  }}
-                >
-                  <Icon />
-                </ActionIcon>
-              ))}
+              <ActionIconLinks
+                name={name}
+                value={links}
+                actionIconProps={{ color: 'gray', size: 50 }}
+              />
             </Group>
           </Card.Section>
         </Stack>
