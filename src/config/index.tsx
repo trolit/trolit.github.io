7dedf9259af5163fc66e68975be250788a49c5b4
pawel idzikowski <iddziku@gmail.com>
2022-12-20 21:55:47 +0100

    Update index.tsx

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -6,7 +6,7 @@ export const BACKGROUND_SRC: string = require('@/assets/media/background.jpg');
 
 export const DEFAULT_IMAGE_SRC: string = require('@/assets/media/projects/default.jpg');
 
-export const PRIMARY_COLOR: MantineColor = 'teal';
+export const PRIMARY_COLOR: MantineColor = 'indigo';
 
 // @NOTE Each color has 10 shades. All colors should always have 10 shades.
 export const PRIMARY_COLOR_SHADE = 8;
