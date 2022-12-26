bce06a0812d9924c65fec0af5decf9cdcb60e7d4
pawel idzikowski <iddziku@gmail.com>
2022-12-27 00:40:19 +0100

    Remove redundant "mode" param from sort

--- src/helpers/sortByDate.ts
+++ src/helpers/sortByDate.ts
@@ -4,11 +4,11 @@ interface IElement {
   date: string;
 }
 
-export function sortByDate<T>(collection: T[], mode: 'desc' | 'asc' = 'desc') {
+export function sortByDate<T>(collection: T[]) {
   return [...collection].sort((firstItem: T, secondItem: T) => {
     const { date: first } = firstItem as IElement;
     const { date: second } = secondItem as IElement;
 
-    return dateSort(first, second, mode);
+    return dateSort(first, second);
   });
 }
