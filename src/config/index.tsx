3fce54be7d8781d768982148aa0062dc83295565
ⓘ p4w31 !d21k0w541 <iddziku@gmail.com>
2022-12-27 00:45:46 +0100

    Feature/minor changes (#82)
    * Update Thumbnail.tsx
    
    * Rename global date format variable
    
    * Rename home date formatter variable
    
    * Remove redundant "mode" param from sort
    
    * Update index.tsx

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -2,16 +2,16 @@ import { OpUnitType } from 'dayjs';
 import { MantineColor } from '@mantine/core';
 import { HomeInterestPoints } from '@/utilities/types';
 
-export const BACKGROUND_SRC: string = require('@/assets/media/background.jpg');
-
-export const DEFAULT_IMAGE_SRC: string = require('@/assets/media/projects/default.jpg');
-
 export const PRIMARY_COLOR: MantineColor = 'teal';
 
 // @NOTE Each color has 10 shades. All colors should always have 10 shades.
 export const LIGHT_THEME_COLOR_SHADE = 8;
 
 export const DARK_THEME_COLOR_SHADE = 9;
+
+export const BACKGROUND_SRC: string = require('@/assets/media/background.jpg');
+
+export const DEFAULT_IMAGE_SRC: string = require('@/assets/media/projects/default.jpg');
 
 export const HEADER_HEIGHT: string = '50px';
 
@@ -23,9 +23,9 @@ export const DASHBOARD_PADDING: number = 20;
 export const GITHUB_USERNAME: string = 'trolit';
 
 // @NOTE https://day.js.org/docs/en/display/format
-export const DATE_FORMAT: string = 'DD/MM/YYYY';
+export const GLOBAL_DATE_FORMAT: string = 'DD/MM/YYYY';
 
-export const HOME_DATE_FORMATTER: string = 'YYYY';
+export const HOME_DATE_FORMAT: string = 'YYYY';
 
 export const HOME_GROUP_BY: OpUnitType = 'year';
 
@@ -35,4 +35,5 @@ export const MAX_HOME_INTEREST_POINTS: HomeInterestPoints = 4;
 
 export const YOUTUBE_WATCH_URL: string = `https://www.youtube.com/watch?v=`;
 
+// @NOTE embed/<video-id>
 export const YOUTUBE_EMBED_URL: string = `https://www.youtube.com/embed`;
--- src/utilities/dateSort.ts
+++ src/utilities/dateSort.ts
@@ -1,9 +1,5 @@
 import dayjs from 'dayjs';
 
-export const dateSort = (a: string, b: string, mode: 'desc' | 'asc' = 'desc') => {
-  if (mode === 'desc') {
-    return dayjs(a).isBefore(dayjs(b)) ? 1 : -1;
-  }
-
-  return dayjs(a).isAfter(dayjs(b)) ? 1 : -1;
+export const dateSort = (a: string, b: string) => {
+  return dayjs(a).isBefore(dayjs(b)) ? 1 : -1;
 };
