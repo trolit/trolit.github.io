a9ff9df1b13ccc05082e9c69f12bcc810e8b0961
pawel idzikowski <iddziku@gmail.com>
2023-02-26 10:51:32 +0100

    Update index.tsx

--- src/components/Dashboard/Post/index.tsx
+++ src/components/Dashboard/Post/index.tsx
@@ -1,39 +1,33 @@
+import { Navigate } from 'react-router-dom';
 import { ScrollArea, Stack } from '@mantine/core';
-import { useParams, Navigate } from 'react-router-dom';
 
 import { POSTS } from '@/assets/data/posts';
-import { IPost } from '@/interfaces/dashboard/IPost';
 import { POSTS_ROUTE } from '@/assets/constants/routes';
 import { usePostRenderer } from '@/services/usePostRenderer';
 import { usePostStyles } from '@/assets/styles/dashboard/post';
 import { useDashboardStyles } from '@/assets/styles/dashboard';
+import { IExtendedPost } from '@/interfaces/dashboard/IExtendedPost';
 
 export function Post() {
-  type ExpectedParams = {
-    postId: string;
-  };
-
   const { wrapper } = usePostStyles();
 
   const { scrollArea } = useDashboardStyles();
 
-  const postRenderer = usePostRenderer<IPost>();
+  const postRenderer = usePostRenderer<IExtendedPost>();
 
-  const { postId } = useParams<keyof ExpectedParams>() as ExpectedParams;
+  const pathname = window.location.pathname;
 
-  const parsedPostId = parseInt(postId);
+  const postIndex = POSTS.findIndex((post) => post.address === pathname);
 
-  if (Number.isNaN(parsedPostId) || parsedPostId < 1 || parsedPostId > POSTS.length) {
-    return <Navigate to={POSTS_ROUTE} replace={true} />;
+  if (~postIndex) {
+    return (
+      <ScrollArea className={scrollArea}>
+        <Stack spacing='xl' align='center' className={wrapper}>
+          {postRenderer.render(POSTS[postIndex])}
+        </Stack>
+      </ScrollArea>
+    );
   }
 
-  const post = POSTS[parsedPostId - 1];
-
-  return (
-    <ScrollArea className={scrollArea}>
-      <Stack spacing='xl' align='center' className={wrapper}>
-        {postRenderer.render(post)}
-      </Stack>
-    </ScrollArea>
-  );
+  return <Navigate to={POSTS_ROUTE} replace={true} />;
 }
