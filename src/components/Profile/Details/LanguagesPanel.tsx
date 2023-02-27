680c3a8c0c8e1f2fb780f4a45920435bdc813814
pawel idzikowski <iddziku@gmail.com>
2023-02-27 22:57:27 +0100

    Update LanguagesPanel.tsx

--- src/components/Profile/Details/LanguagesPanel.tsx
+++ src/components/Profile/Details/LanguagesPanel.tsx
@@ -1,3 +1,14 @@
+import { Group } from '@mantine/core';
+
+import { tags } from '@/assets/data/profile';
+import { Tags } from '@/components/Dashboard/common/Tags';
+
 export function LanguagesPanel() {
-  return <>12</>;
+  return (
+    <Group>
+      <Group position='center' spacing='lg'>
+        <Tags name='profile' value={tags} badgeProps={{ size: 'md', radius: 'xs' }} />
+      </Group>
+    </Group>
+  );
 }
