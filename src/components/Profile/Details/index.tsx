489be2b65f6e7e24b313d041c69eba5fea53960b
pawel idzikowski <iddziku@gmail.com>
2023-02-27 22:08:16 +0100

    Init `Details`

---
+++ src/components/Profile/Details/index.tsx
@@ -0,0 +1,40 @@
+import { Tabs } from '@mantine/core';
+
+import { VscPanel } from './VscPanel';
+import { AboutPanel } from './AboutPanel';
+import { LanguagesPanel } from './LanguagesPanel';
+
+export function Details() {
+  const aboutPanel = {
+    name: 'About',
+    children: AboutPanel,
+  };
+
+  const languagesPanel = {
+    name: 'Languages',
+    children: LanguagesPanel,
+  };
+
+  const vscPanel = {
+    name: 'VSC Setup',
+    children: VscPanel,
+  };
+
+  const panels = [aboutPanel, languagesPanel, vscPanel];
+
+  return (
+    <Tabs defaultValue={aboutPanel.name.toLowerCase()} h={200}>
+      <Tabs.List grow position='center'>
+        {panels.map(({ name }) => (
+          <Tabs.Tab key={`tabs-list-${name}`} value={name.toLowerCase()}>
+            {name}
+          </Tabs.Tab>
+        ))}
+      </Tabs.List>
+
+      {panels.map(({ name, children: Children }) => (
+        <Tabs.Panel key={`tabs-panel-${name}`} value={name.toLowerCase()} children={<Children />} p='lg' pt='xs' />
+      ))}
+    </Tabs>
+  );
+}
