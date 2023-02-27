c1459da5028577780e5baea9f825b48bd8287b10
pawel idzikowski <iddziku@gmail.com>
2023-02-27 22:57:31 +0100

    Update AboutPanel.tsx

--- src/components/Profile/Details/AboutPanel.tsx
+++ src/components/Profile/Details/AboutPanel.tsx
@@ -1,3 +1,18 @@
+import { Text, Anchor } from '@mantine/core';
+
 export function AboutPanel() {
-  return <>00</>;
+  return (
+    <Text align='justify' fz='sm'>
+      IT branch passionate. Started from playing around with{' '}
+      <Anchor href='https://forums.alliedmods.net/showthread.php?t=62967' target='_blank'>
+        Necro's blockmaker plugin
+      </Anchor>{' '}
+      through{' '}
+      <Anchor href='https://gamebanana.com/mods/92118' target='_blank'>
+        level design
+      </Anchor>
+      , making first steps in Unity 3.x, simple page (Przepisownik 2016). Active source code investigator since the end
+      of year 2017. Visual Studio Code and Visual Studio are my favourite IDEs.
+    </Text>
+  );
 }
