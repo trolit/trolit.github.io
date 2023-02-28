09a4da09e3ba60fcecea3b8d35f362cdc9322f70
pawel idzikowski <iddziku@gmail.com>
2023-02-28 21:58:49 +0100

    Update AboutPanel.tsx

--- src/components/Profile/Details/AboutPanel.tsx
+++ src/components/Profile/Details/AboutPanel.tsx
@@ -3,16 +3,28 @@ import { Text, Anchor } from '@mantine/core';
 export function AboutPanel() {
   return (
     <Text align='justify' fz='sm'>
-      IT branch passionate. Started from playing around with{' '}
+      Passionate about computers. Started from playing around with{' '}
       <Anchor href='https://forums.alliedmods.net/showthread.php?t=62967' target='_blank'>
         Necro's blockmaker plugin
-      </Anchor>{' '}
-      through{' '}
+      </Anchor>
+      {' in 2008, '}
+      through some{' '}
       <Anchor href='https://gamebanana.com/mods/92118' target='_blank'>
         level design
+      </Anchor>{' '}
+      experience, making first steps in{' '}
+      <Anchor
+        href='https://helion.pl/ksiazki/projektowanie-gier-w-srodowisku-unity-3-x-will-goldstone,prgun3.htm#format/e'
+        target='_blank'
+      >
+        Unity 3.x
+      </Anchor>{' '}
+      , creating first page (
+      <Anchor href='http://przepisownik2016.opx.pl/' target='_blank'>
+        Przepisownik 2016
       </Anchor>
-      , making first steps in Unity 3.x, simple page (Przepisownik 2016). Active source code investigator since the end
-      of year 2017. Visual Studio Code and Visual Studio are my favourite IDEs.
+      ). Active source code investigator since the end of the year 2017. Visual Studio Code and Visual Studio are
+      favourite IDEs.
     </Text>
   );
 }
