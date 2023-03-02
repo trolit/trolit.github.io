b24d8bce307c9729a2429c6539494f7e7670376d
ⓘ p4w31 !d21k0w541 <iddziku@gmail.com>
2023-03-02 20:55:22 +0100

    Feature/extract tabs data to profile data module (#99)
    * create `ABOUT_ME_PANEL_DATA` constant
    
    * use `reactStringReplace` in `AboutPanel`
    
    * Update LanguagesPanel.tsx
    
    * Update LanguagesPanel.tsx
    
    * extract `VSC_PANEL_DATA`

--- src/assets/data/profile.ts
+++ src/assets/data/profile.ts
@@ -15,6 +15,29 @@ export const NAME = 'p4w31 !d21k0w541';
 
 export const TITLE = 'code investigator';
 
+export const ABOUT_ME_PANEL_DATA = {
+  text: `Passionate about computers. Started from playing around with Necro's blockmaker plugin in 2008, through some level design, making first steps in Unity 3.x, creating first page (Przepisownik 2016). Active source code investigator since the end of the year 2017. Visual Studio Code and Visual Studio stand for favourite IDEs.`,
+
+  links: [
+    {
+      match: `Necro's blockmaker plugin`,
+      url: 'https://forums.alliedmods.net/showthread.php?t=62967',
+    },
+    {
+      match: 'level design',
+      url: 'https://gamebanana.com/mods/92118',
+    },
+    {
+      match: 'Unity 3.x',
+      url: 'https://helion.pl/ksiazki/projektowanie-gier-w-srodowisku-unity-3-x-will-goldstone,prgun3.htm#format/e',
+    },
+    {
+      match: 'Przepisownik 2016',
+      url: 'http://przepisownik2016.opx.pl/',
+    },
+  ],
+};
+
 export const LANGUAGES_PANEL_DATA = [
   {
     title: 'favourite',
@@ -23,6 +46,43 @@ export const LANGUAGES_PANEL_DATA = [
   {
     title: 'also worked with',
     data: [KOTLIN_LANGUAGE, JAVA_LANGUAGE],
+  },
+];
+
+export const VSC_PANEL_DATA = [
+  {
+    name: 'CSS class names IntelliSense in HTML',
+    url: 'https://marketplace.visualstudio.com/items?itemName=Zignd.html-css-class-completion',
+  },
+
+  {
+    name: 'Better Comments',
+    url: 'https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments',
+  },
+
+  {
+    name: 'GitHub Theme',
+    url: 'https://marketplace.visualstudio.com/items?itemName=GitHub.github-vscode-theme',
+  },
+
+  {
+    name: 'GitLens',
+    url: 'https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens',
+  },
+
+  {
+    name: 'Turbo Console Log',
+    url: 'https://marketplace.visualstudio.com/items?itemName=ChakrounAnas.turbo-console-log',
+  },
+
+  {
+    name: 'Auto Rename Tag',
+    url: 'https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-rename-tag',
+  },
+
+  {
+    name: 'Auto Close Tag',
+    url: 'https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-close-tag',
   },
 ];
 
--- src/components/Profile/Details/VscPanel.tsx
+++ src/components/Profile/Details/VscPanel.tsx
@@ -1,48 +1,13 @@
 import { Group, Anchor } from '@mantine/core';
 
+import { VSC_PANEL_DATA } from '@/assets/data/profile';
+
 export function VscPanel() {
-  const config = [
-    {
-      name: 'CSS class names IntelliSense in HTML',
-      url: 'https://marketplace.visualstudio.com/items?itemName=Zignd.html-css-class-completion',
-    },
-
-    {
-      name: 'Better Comments',
-      url: 'https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments',
-    },
-
-    {
-      name: 'GitHub Theme',
-      url: 'https://marketplace.visualstudio.com/items?itemName=GitHub.github-vscode-theme',
-    },
-
-    {
-      name: 'GitLens',
-      url: 'https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens',
-    },
-
-    {
-      name: 'Turbo Console Log',
-      url: 'https://marketplace.visualstudio.com/items?itemName=ChakrounAnas.turbo-console-log',
-    },
-
-    {
-      name: 'Auto Rename Tag',
-      url: 'https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-rename-tag',
-    },
-
-    {
-      name: 'Auto Close Tag',
-      url: 'https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-close-tag',
-    },
-  ];
-
   return (
     <>
       <Group position='center'>
-        {config.map(({ name, url }) => (
-          <Anchor key={name} size='sm' href={url} target='_blank'>
+        {VSC_PANEL_DATA.map(({ name, url }) => (
+          <Anchor key={name} size='md' href={url} target='_blank'>
             {name}
           </Anchor>
         ))}
