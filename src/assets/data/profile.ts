14adb16708ae602683e1cfda76f71063a93f8b82
pawel idzikowski <iddziku@gmail.com>
2023-03-02 20:43:32 +0100

    create `ABOUT_ME_PANEL_DATA` constant

--- src/assets/data/profile.ts
+++ src/assets/data/profile.ts
@@ -15,6 +15,29 @@ export const NAME = 'p4w31 !d21k0w541';
 
 export const TITLE = 'code investigator';
 
+export const ABOUT_ME_PANEL_DATA = {
+  text: `Passionate about computers. Started from playing around with Necro's blockmaker plugin in 2008, through some level design, making first steps in Unity 3.x, creating first page (Przepisownik 2016). Active source code investigator since the end of the year 2017. Visual Studio Code and Visual Studio stand for favourite IDEs.`,
+
+  links: [
+    {
+      match: `Necro's blockmaker plugin`,
+      url: 'https://forums.alliedmods.net/showthread.php?t=62967',
+    },
+    {
+      match: 'level design',
+      url: 'https://gamebanana.com/mods/92118',
+    },
+    {
+      match: 'Unity 3.x',
+      url: 'https://helion.pl/ksiazki/projektowanie-gier-w-srodowisku-unity-3-x-will-goldstone,prgun3.htm#format/e',
+    },
+    {
+      match: 'Przepisownik 2016',
+      url: 'http://przepisownik2016.opx.pl/',
+    },
+  ],
+};
+
 export const LANGUAGES_PANEL_DATA = [
   {
     title: 'favourite',
