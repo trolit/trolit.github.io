cbb552f14a0cf5e088cfff75a32f060db5f6c915
pawel idzikowski <iddziku@gmail.com>
2023-03-02 20:53:12 +0100

    extract `VSC_PANEL_DATA`

--- src/assets/data/profile.ts
+++ src/assets/data/profile.ts
@@ -49,6 +49,43 @@ export const LANGUAGES_PANEL_DATA = [
   },
 ];
 
+export const VSC_PANEL_DATA = [
+  {
+    name: 'CSS class names IntelliSense in HTML',
+    url: 'https://marketplace.visualstudio.com/items?itemName=Zignd.html-css-class-completion',
+  },
+
+  {
+    name: 'Better Comments',
+    url: 'https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments',
+  },
+
+  {
+    name: 'GitHub Theme',
+    url: 'https://marketplace.visualstudio.com/items?itemName=GitHub.github-vscode-theme',
+  },
+
+  {
+    name: 'GitLens',
+    url: 'https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens',
+  },
+
+  {
+    name: 'Turbo Console Log',
+    url: 'https://marketplace.visualstudio.com/items?itemName=ChakrounAnas.turbo-console-log',
+  },
+
+  {
+    name: 'Auto Rename Tag',
+    url: 'https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-rename-tag',
+  },
+
+  {
+    name: 'Auto Close Tag',
+    url: 'https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-close-tag',
+  },
+];
+
 export const LINKS: ILink[] = [GITHUB_LINK()];
 
 export const QUOTE: IQuote = {
