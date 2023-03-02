2d43bdfc4322b19c7e79897e460913ffdbd6a2eb
pawel idzikowski <iddziku@gmail.com>
2023-03-02 14:33:23 +0100

    update profile data

--- src/assets/data/profile.ts
+++ src/assets/data/profile.ts
@@ -1,19 +1,28 @@
-import { ITag } from '@/interfaces/ITag';
 import { ILink } from '@/interfaces/ILink';
+import { ILanguage } from '@/interfaces/ILanguage';
 import { IQuote } from '@/interfaces/profile/IQuote';
 import { GITHUB_LINK } from '@/assets/constants/predefined-links';
+import {
+  JAVA_LANGUAGE,
+  CSHARP_LANGUAGE,
+  KOTLIN_LANGUAGE,
+  TYPESCRIPT_LANGUAGE,
+  JAVASCRIPT_LANGUAGE,
+} from '@/assets/constants/predefined-languages';
 
-export const avatarSrc = require('@/assets/media/avatar.jpg');
+export const PATH_TO_AVATAR = require('@/assets/media/avatar.jpg');
 
-export const name = 'p4w31 !d21k0w541';
+export const NAME = 'p4w31 !d21k0w541';
 
-export const title = 'code investigator';
+export const TITLE = 'code investigator';
 
-export const tags: ITag[] = [{ text: 'C#' }, { text: 'JavaScript' }, { text: 'TypeScript' }];
+export const FAVOURITE_LANGUAGES: ILanguage[] = [CSHARP_LANGUAGE, TYPESCRIPT_LANGUAGE, JAVASCRIPT_LANGUAGE];
 
-export const links: ILink[] = [GITHUB_LINK('')];
+export const OTHER_LANGUAGES: ILanguage[] = [KOTLIN_LANGUAGE, JAVA_LANGUAGE];
 
-export const quote: IQuote = {
+export const LINKS: ILink[] = [GITHUB_LINK('')];
+
+export const QUOTE: IQuote = {
   author: 'Thomas Edison',
   text: "I have not failed. I just found 10,000 ways that won't work.",
 };
