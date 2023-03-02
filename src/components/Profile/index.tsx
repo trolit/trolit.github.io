bf3394cee6aabef40c068e80a7899da13ba3bd07
pawel idzikowski <iddziku@gmail.com>
2023-03-02 13:19:32 +0100

    update profile component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
-import { Kbd, Text, Group, Paper, Stack, Avatar, ThemeIcon, Blockquote, Anchor, Button, Divider } from '@mantine/core';
+import { IconBadges } from '@tabler/icons';
+import { Kbd, Text, Group, Paper, Avatar, ThemeIcon, Blockquote, Anchor, Divider } from '@mantine/core';
 
 import { RootState } from '@/store';
 import { Details } from './Details';
@@ -12,55 +13,47 @@ export function Profile() {
 
   const shadedColor = useSelector((state: RootState) => state.preferences.shadedColor);
 
-  const { wrapper, titleWrapper, link, blockQuote } = useProfileStyles();
+  const { wrapper, titleWrapper, link, blockQuote, presentationGroup, linksGroup } = useProfileStyles();
 
   return (
     <Paper p={0} radius='md' withBorder className={`${panel} ${wrapper} ${h100}`}>
-      <Stack justify='space-between' className={h100}>
-        <Group grow p='lg' pb={0} noWrap>
-          <Avatar src={avatarSrc} alt={name} size={130} radius={50} mx='auto' />
-
-          <div>
-            <Text align='center' size='lg' weight={500} mb={8}>
-              {name}
-            </Text>
-
-            {title && (
-              <Text className={titleWrapper} lineClamp={1}>
-                <Kbd>{title}</Kbd>
-              </Text>
-            )}
-          </div>
-        </Group>
-
-        <Details />
-
-        <Blockquote className={blockQuote} cite={`-${quote.author}`} color={shadedColor}>
-          {quote.text}
-        </Blockquote>
-
-        <Group px='lg' grow>
-          <Button variant='default'>timeline</Button>
-        </Group>
+      <Group grow p='lg' noWrap className={presentationGroup}>
+        <Avatar src={avatarSrc} alt={name} size={130} radius={40} mx='auto' />
 
         <div>
-          <Divider color='dark.5' />
+          <Text align='center' size='lg' weight={500} mb={8}>
+            {name}
+          </Text>
 
-          <Group py={10} position='center' spacing='lg'>
-            {links.map(({ icon, url }, index) => {
-              const LinkIcon = icon;
-
-              return (
-                <Anchor key={index} href={url} target='_blank'>
-                  <ThemeIcon size={45} radius='xs' variant='light' className={link}>
-                    <LinkIcon />
-                  </ThemeIcon>
-                </Anchor>
-              );
-            })}
-          </Group>
+          {title && (
+            <Text className={titleWrapper} lineClamp={1}>
+              <IconBadges /> &nbsp; <Kbd>{title}</Kbd>
+            </Text>
+          )}
         </div>
-      </Stack>
+      </Group>
+
+      <Details />
+
+      <Divider />
+
+      <Blockquote className={blockQuote} cite={`-${quote.author}`} color={shadedColor}>
+        {quote.text}
+      </Blockquote>
+
+      <Group position='center' spacing='lg' className={linksGroup}>
+        {links.map(({ icon, url }, index) => {
+          const LinkIcon = icon;
+
+          return (
+            <Anchor key={index} href={url} target='_blank'>
+              <ThemeIcon size={40} radius='xs' variant='default' className={link}>
+                <LinkIcon />
+              </ThemeIcon>
+            </Anchor>
+          );
+        })}
+      </Group>
     </Paper>
   );
 }
