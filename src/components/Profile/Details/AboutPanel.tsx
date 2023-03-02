447b43e612e6c87eea2ef98a656ef80dbee4cc90
pawel idzikowski <iddziku@gmail.com>
2023-03-02 20:43:44 +0100

    use `reactStringReplace` in `AboutPanel`

--- src/components/Profile/Details/AboutPanel.tsx
+++ src/components/Profile/Details/AboutPanel.tsx
@@ -1,30 +1,29 @@
+import { ReactNode } from 'react';
 import { Text, Anchor } from '@mantine/core';
+import reactStringReplace from 'react-string-replace';
+
+import { ABOUT_ME_PANEL_DATA } from '@/assets/data/profile';
+
+function getTextWithAnchors() {
+  const { text, links } = ABOUT_ME_PANEL_DATA;
+
+  let filledText: ReactNode[] = [];
+
+  for (const { match, url } of links) {
+    filledText = reactStringReplace(filledText.length ? filledText : text, match, () => (
+      <Anchor key={`${match}-url`} href={url} target='_blank'>
+        {match}
+      </Anchor>
+    )) as ReactNode[];
+  }
+
+  return filledText;
+}
 
 export function AboutPanel() {
   return (
     <Text align='justify' fz='sm'>
-      Passionate about computers. Started from playing around with{' '}
-      <Anchor href='https://forums.alliedmods.net/showthread.php?t=62967' target='_blank'>
-        Necro's blockmaker plugin
-      </Anchor>
-      {' in 2008, '}
-      through some{' '}
-      <Anchor href='https://gamebanana.com/mods/92118' target='_blank'>
-        level design
-      </Anchor>
-      , making first steps in{' '}
-      <Anchor
-        href='https://helion.pl/ksiazki/projektowanie-gier-w-srodowisku-unity-3-x-will-goldstone,prgun3.htm#format/e'
-        target='_blank'
-      >
-        Unity 3.x
-      </Anchor>
-      , creating first page (
-      <Anchor href='http://przepisownik2016.opx.pl/' target='_blank'>
-        Przepisownik 2016
-      </Anchor>
-      ). Active source code investigator since the end of the year 2017. Visual Studio Code and Visual Studio stand for
-      favourite IDEs.
+      {getTextWithAnchors()}
     </Text>
   );
 }
