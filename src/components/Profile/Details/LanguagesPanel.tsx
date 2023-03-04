251983bfcb6ff7fcbfdf0d2657484a23abe7eef4
ⓘ p4w31 !d21k0w541 <iddziku@gmail.com>
2023-03-05 00:29:33 +0100

    Feature/clickable common component language badges (#108)
    * Update Languages.tsx
    
    * make language badges in profile card panel - clickable
    
    * Update Languages.tsx
    
    * on profile card badge click - dispatch to "All tab" as well

--- src/components/Profile/Details/LanguagesPanel.tsx
+++ src/components/Profile/Details/LanguagesPanel.tsx
@@ -1,9 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { Group, Text, Stack } from '@mantine/core';
 
+import { ProjectsSegment } from '@/enums/ProjectsSegment';
+import { PROJECTS_ROUTE } from '@/assets/constants/routes';
 import { LANGUAGES_PANEL_DATA } from '@/assets/data/profile';
 import { Languages } from '@/components/Dashboard/common/Languages';
+import { setActiveLanguage, setActiveSegment } from '@/store/projects';
 
 export function LanguagesPanel() {
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
   return (
     <Stack>
       {LANGUAGES_PANEL_DATA.filter((element) => !!element.data.length).map(({ title, data }, index) => (
@@ -14,11 +23,19 @@ export function LanguagesPanel() {
 
           <Group grow>
             <Languages
+              clickable
               displayFullName
               useBadgeColorInHexAsBackground
               name={`${title}-languages`}
               value={data}
               badgeProps={{ size: 'lg', radius: 'sm' }}
+              onBadgeClick={(language) => {
+                dispatch(setActiveLanguage(language.acronym));
+
+                dispatch(setActiveSegment(ProjectsSegment.ALL));
+
+                navigate(PROJECTS_ROUTE);
+              }}
             />
           </Group>
         </div>
