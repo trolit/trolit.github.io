ac456a0d0a9b1ed41593956d81cbe95396334e07
pawel idzikowski <iddziku@gmail.com>
2023-03-05 00:19:56 +0100

    make language badges in profile card panel - clickable

--- src/components/Profile/Details/LanguagesPanel.tsx
+++ src/components/Profile/Details/LanguagesPanel.tsx
@@ -1,9 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { Group, Text, Stack } from '@mantine/core';
 
+import { setActiveLanguage } from '@/store/projects';
 import { LANGUAGES_PANEL_DATA } from '@/assets/data/profile';
 import { Languages } from '@/components/Dashboard/common/Languages';
+import { PROJECTS_ROUTE } from '@/assets/constants/routes';
 
 export function LanguagesPanel() {
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
   return (
     <Stack>
       {LANGUAGES_PANEL_DATA.filter((element) => !!element.data.length).map(({ title, data }, index) => (
@@ -14,11 +22,17 @@ export function LanguagesPanel() {
 
           <Group grow>
             <Languages
+              clickable
               displayFullName
               useBadgeColorInHexAsBackground
               name={`${title}-languages`}
               value={data}
               badgeProps={{ size: 'lg', radius: 'sm' }}
+              onBadgeClick={(language) => {
+                dispatch(setActiveLanguage(language.acronym));
+
+                navigate(PROJECTS_ROUTE);
+              }}
             />
           </Group>
         </div>
