8f4dfccb793db1d1e12e3656f316cef28c596381
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-03 16:16:30 +0100

    Hotfix/fix deploy script (#116)
    * Update deploy.yml
    
    * Update package.json
    
    * Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '',
+  base: '/',
   plugins: [react(), viteTsconfigPaths()],
   server: {
     // this ensures that the browser opens upon server start
