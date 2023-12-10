5e210664880efd551bf45cbe32c4d94e512691fa
p4w31 !d21k0w541 <iddziku@gmail.com>
2023-12-10 22:52:02 +0100

    include post

--- src/assets/data/posts.ts
+++ src/assets/data/posts.ts
@@ -5,11 +5,12 @@ import { POST as POST3 } from './posts/2021-12-27';
 import { POST as POST4 } from './posts/2021-12-29';
 import { POST as POST5 } from './posts/2022-01-09';
 import { POST as POST6 } from './posts/2023-12-04';
+import { POST as POST7 } from './posts/2023-12-11';
 import { POSTS_ROUTE } from '@/assets/constants/routes';
 import { IExtendedPost } from '@/interfaces/dashboard/IExtendedPost';
 import { translateStringToAddress } from '@/helpers/translateStringToAddress';
 
-const RAW_POSTS: IExtendedPost[] = [POST1, POST2, POST3, POST4, POST5, POST6].map(
+const RAW_POSTS: IExtendedPost[] = [POST1, POST2, POST3, POST4, POST5, POST6, POST7].map(
   (post) =>
     ({
       ...post,
