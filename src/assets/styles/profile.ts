8b42d98a9121a94abfdae01572e77f57bb875170
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-02-04 12:09:59 +0100

    Update profile.ts

--- src/assets/styles/profile.ts
+++ src/assets/styles/profile.ts
@@ -1,4 +1,3 @@
-import { OVERLAY_PADDING } from '@/config';
 import { createStyles } from '@mantine/core';
 
 const PRESENTATION_GROUP_HEIGHT = '25vh';
@@ -14,7 +13,6 @@ export const useProfileStyles = () => {
     const classes = {
       wrapper: {
         minWidth: 300,
-        borderRight: OVERLAY_PADDING === 0 ? '1px dashed #3F484B' : '',
       },
 
       presentationGroup: {
