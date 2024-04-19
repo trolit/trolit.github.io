ab1eaf98cb0186f53efb588c953d0ae29fd8e997
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-20 01:33:33 +0200

    add background media

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ const { nextui } = require('@nextui-org/react');
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'home-page': `url('/src/assets/media/main.jpg')`,
+        'content-page': "url('/src/assets/media/sprinkle.svg')",
+      },
+    },
   },
   darkMode: 'class',
   plugins: [nextui()],
