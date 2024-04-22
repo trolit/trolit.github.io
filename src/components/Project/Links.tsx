afd498c91acf0ec1d350bcf274520d7cc626f2d0
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-23 00:11:14 +0200

    Create Links.tsx

---
+++ src/components/Project/Links.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { ILink } from '@/types/ILink';
+import { Accordion, AccordionItem, Chip, Link } from '@nextui-org/react';
+
+interface IProps {
+  identifier: string | number;
+
+  value: ILink[];
+}
+
+export function Links({ identifier, value }: IProps) {
+  const [areLinksVisible, expandLinks] = useState(false);
+
+  return (
+    <Accordion hideIndicator isCompact variant='light'>
+      <AccordionItem
+        key='1'
+        aria-label='View more links'
+        title={
+          <Chip radius='sm' variant='bordered' color={areLinksVisible ? 'default' : 'success'}>
+            More links ({value.length})
+          </Chip>
+        }
+        onPress={() => expandLinks(!areLinksVisible)}
+      >
+        <div className='flex flex-wrap w-full gap-4'>
+          {value.map((link, index) => (
+            <Link isExternal underline='always' key={`${identifier}-link-${index}`} href={link.url} size='md'>
+              {link.name}
+            </Link>
+          ))}
+        </div>
+      </AccordionItem>
+    </Accordion>
+  );
+}
