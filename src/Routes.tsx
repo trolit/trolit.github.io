a69ae6bd34a26c21354a5c0c71a8d1384a05dc12
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-23 23:49:49 +0200

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,27 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { Home } from '@/pages/Home';
-import { HOME_ROUTE } from '@/assets/constants/routes';
+import { Posts } from '@/pages/Posts';
+import { Soundtracks } from '@/pages/Soundtracks';
+import { HOME_ROUTE, SOUNDTRACKS_ROUTE, POSTS_ROUTE, PROJECTS_ROUTE } from '@/assets/constants/routes';
+import { Projects } from '@/pages/Projects';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+
+      <Route path={PROJECTS_ROUTE}>
+        <Route index element={<Projects />} />
+      </Route>
+
+      <Route path={POSTS_ROUTE}>
+        <Route index element={<Posts />} />
+      </Route>
+
+      <Route path={SOUNDTRACKS_ROUTE}>
+        <Route index element={<Soundtracks />} />
+      </Route>
 
       <Route path='*' element={<Navigate replace to={HOME_ROUTE} />} />
     </Routes>
