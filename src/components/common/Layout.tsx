1f0c5988e7d90e9b0a77413758f69e976923f772
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-24 00:13:11 +0200

    set min height of layout subtracting navbar height

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,7 +1,9 @@
 export function Layout({ children }: any) {
   return (
-    <div className='h-full bg-fixed bg-no-repeat bg-content-page'>
-      <div className={`grid w-11/12 sm:w-9/12 gap-4 py-4 mx-auto shadow-xl bg-neutral-50 dark:bg-gray-950 text-pretty`}>
+    <div className='bg-fixed bg-no-repeat bg-content-page'>
+      <div
+        className={`min-h-[calc(100vh-3rem)] grid w-11/12 sm:w-9/12 gap-4 py-4 mx-auto shadow-xl bg-neutral-50 dark:bg-gray-950 text-pretty`}
+      >
         {children}
       </div>
     </div>
