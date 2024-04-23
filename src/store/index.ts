87947c38a4d73daa5676f6cb1fa268d9d58e775b
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-23 23:49:43 +0200

    re-add general store

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import generalReducer from './general';
+
+export const store = configureStore({
+  reducer: {
+    general: generalReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
