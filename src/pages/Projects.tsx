424d58ad0ea59935b654f325fa3ce13158485213
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-23 16:46:42 +0200

    Create Projects.tsx

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,23 @@
+import PROJECTS from '@/data/projects';
+import { IProject } from '@/types/IProject';
+import { Project } from '@/components/Project';
+import { sortByDate } from '@/helpers/sortByDate';
+import { Layout } from '@/components/common/Layout';
+
+export function Projects() {
+  const sortedProjects: IProject[] = sortByDate<IProject>(PROJECTS);
+
+  return (
+    <Layout>
+      <div className='grid gap-5'>
+        {sortedProjects.map((project, index) => {
+          return (
+            <div key={`project-${index}`} className={`col-span-12 ${project.wrapperClassName || 'sm:col-span-6'}`}>
+              <Project value={project} />
+            </div>
+          );
+        })}
+      </div>
+    </Layout>
+  );
+}
