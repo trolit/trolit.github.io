ae1358d9d9b7b141f3eac481833b57b1478af7fa
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-24 23:03:29 +0200

    rework Header

---
+++ src/services/useContentRenderer/interfaces/IHeader.ts
@@ -0,0 +1,5 @@
+export interface IHeader {
+  value: string;
+
+  className?: string;
+}
---
+++ src/services/useContentRenderer/renderers/renderHeader.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+import { Header } from '../components';
+
+export function renderHeader(index: number, header: Header): ReactNode {
+  const {
+    data: { value, className },
+  } = header;
+
+  return (
+    <div
+      key={index}
+      className={`tracking-tight inline font-semibold from-indigo-500 to-indigo-600 text-[3rem] bg-clip-text text-transparent bg-gradient-to-b ${className}`}
+    >
+      {value}
+    </div>
+  );
+}
