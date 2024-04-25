aee951c6a128cf9c6fc9bf2e775f1180a51afbfb
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-25 22:43:59 +0200

    handle post button press

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,17 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import { POSTS } from '@/data/posts';
-import { IPost } from '@/types/IPost';
 import { Post } from '@/components/Post';
 import { sortByDate } from '@/helpers/sortByDate';
 import { Layout } from '@/components/common/Layout';
+import { IExtendedPost } from '@/types/IExtendedPost';
 
 export function Posts() {
-  const sortedPosts = sortByDate<IPost>(POSTS);
+  const navigate = useNavigate();
+  const sortedPosts = sortByDate<IExtendedPost>(POSTS);
 
   return (
     <Layout>
       {sortedPosts.map((post, index) => (
         <div key={`post-${index}`} className='px-4'>
-          <Post value={post} />
+          <Post value={post} onPress={() => navigate(post.address)} />
         </div>
       ))}
     </Layout>
