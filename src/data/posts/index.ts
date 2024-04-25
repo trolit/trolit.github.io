6e5f7e9a24fa15bd2e8fe2aada79a3e0564f880b
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-25 22:49:27 +0200

    include second post

--- src/data/posts/index.ts
+++ src/data/posts/index.ts
@@ -3,8 +3,9 @@ import { POSTS_ROUTE } from '@/assets/constants/routes';
 import { translateStringToAddress } from '@/helpers/translateStringToAddress';
 
 import { POST as POST1 } from './2020-02-22';
+import { POST as POST2 } from './2020-03-06';
 
-export const POSTS: IExtendedPost[] = [POST1].map((post) => ({
+export const POSTS: IExtendedPost[] = [POST1, POST2].map((post) => ({
   ...post,
   address: translateStringToAddress(post.name, POSTS_ROUTE),
 }));
