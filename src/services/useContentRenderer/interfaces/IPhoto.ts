bbc2d649dc5e1688abebc7c0684be8a02d8f51ba
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-26 10:18:07 +0200

    add option to modify width/height of image

--- src/services/useContentRenderer/interfaces/IPhoto.ts
+++ src/services/useContentRenderer/interfaces/IPhoto.ts
@@ -1,6 +1,10 @@
 export interface IPhoto {
   src: string;
 
+  height?: number;
+
+  width?: number;
+
   description: string;
 
   showDescription?: boolean;
--- src/services/useContentRenderer/renderers/renderPhoto.tsx
+++ src/services/useContentRenderer/renderers/renderPhoto.tsx
@@ -6,24 +6,33 @@ import { Image, Link } from '@nextui-org/react';
 export function renderPhoto(index: number, photo: Photo): ReactNode {
   const { data } = photo;
 
-  const { className, src, description, sourceUrl, showDescription } = data;
+  const { height, width, className, src, description, sourceUrl, showDescription } = data;
 
   return (
     <div key={index}>
-      <Image className={className} src={src} alt={description} aria-placeholder={description} />
+      <div className='flex flex-col items-center justify-center'>
+        <Image
+          width={width}
+          height={height}
+          className={className}
+          src={src}
+          alt={description}
+          aria-placeholder={description}
+        />
 
-      {!!sourceUrl ||
-        (showDescription && (
-          <div className='text-right'>
-            {showDescription ? (
-              `${description}`
-            ) : (
-              <Link isExternal href={sourceUrl} color='secondary'>
-                (source)
-              </Link>
-            )}
-          </div>
-        ))}
+        {!!sourceUrl ||
+          (showDescription && (
+            <div className='text-right'>
+              {showDescription ? (
+                `${description}`
+              ) : (
+                <Link isExternal href={sourceUrl} color='secondary'>
+                  (source)
+                </Link>
+              )}
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
