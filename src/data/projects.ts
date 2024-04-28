debd12cc05a94b2a07f6e36fb3f7e594625f81a3
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-04-29 00:43:56 +0200

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -43,7 +43,7 @@ const PROJECTS: IProject[] = [
     ],
     youTubeId: 'aJ9LEZ9TF4k',
     links: [],
-    description: `Web app made from scratch, inteded to serve as source code intelligence "bank" 🏛️. It allows to save purpose of uploaded project(s) source code and extract chosen parts by generating zipped bundle. Due to dealing with sensitive data, it was built in a way to provide advanced access control over app (granular permissions system, workspace insights, data flow "footprints", sessions management). Apart of security measures it also provides implementation of real-time communication made with Engine.io, mailing system, global chat, option to store files locally or in S3 cloud (with option to easily implement own provider) and custom API ("Probata") for defining integration tests which are ran over testcontainers.`,
+    description: `Source code intelligence "bank" 🏛️. Allows to save purpose of uploaded project(s) source code and extract chosen parts on demand. Due to dealing with sensitive data, it was built in a way to provide advanced access control over app (granular permissions system, workspace insights, data flow "footprints", sessions management). It also provides implementation of real-time communication made with Engine.io, mailing system, global chat, option to store files locally or in S3 cloud (with option to easily add own provider) and custom API ("Probata") for defining integration tests which are ran over testcontainers.`,
   },
 
   {
@@ -62,7 +62,7 @@ const PROJECTS: IProject[] = [
       },
     ],
     description:
-      'Bot made with Probot framework to support (not substitute) developers by making initial pull request code review. Can be used to e.g. watch over code conventions so reviewers can focus on other aspects of code. It runs set of rules (configured via JSON file) against hooked pull request data. Rules can be easily created via base class (see docs) plus bot can be ran via GitHub Actions.',
+      'Bot to support (not substitute) developers by making initial pull request code review. Can be used to e.g. watch over agreed code conventions so reviewers can focus on more important aspects of code. It runs set of rules (configured via JSON file) against hooked pull request data. Rules can be easily added via base class plus bot can be ran via GitHub Actions.',
   },
 
   {
@@ -80,7 +80,7 @@ const PROJECTS: IProject[] = [
         url: getYouTubeUrl('UtQ0g11_Juc').toString(),
       },
     ],
-    description: `Top down view, educational, sandbox game with RPG elements about programming. The main motivation behind taking Unity was to face up challenge to create something complex (because in childhood I've attempted to learn Unity 3.x 🤪). It has 40 unique levels (5 lands x 8 levels), 4 minigames, character creation, NPCs interaction, dev console, installer, rich documentation and many more!`,
+    description: `Top down view, educational, sandbox game with RPG elements about programming. The main motivation behind taking Unity was to face up challenge to create something complex (in childhood I've attempted to learn Unity 3.x 🤪). It has 40 unique levels (5 lands x 8 levels), 4 minigames, character creation, NPCs interaction, dev console, installer, rich documentation and much more!`,
   },
 
   {
@@ -103,7 +103,7 @@ const PROJECTS: IProject[] = [
       },
     ],
     description:
-      'WYSIWYG tool 🪛 made with JS and JQuery to setup & generate interesting templates of markdown files (e.g. repository specifications). First, stable version was released in 6 days and afterwards it received total of 16 updates including settings, different themes, modal that allows to quickly jump to selected element or quickly swap two elements (so user does not have to drag element past N other components 😴).',
+      'WYSIWYG tool 🪛 created with JS and JQuery to setup & generate interesting templates of markdown files (e.g. repository specifications). First, stable version was released in 6 days and afterwards it received total of 16 updates including settings,  different themes, modal that allows to quickly jump to selected element or quickly swap two elements (so user does not have to drag element past N other components 😴).',
   },
 
   {
@@ -115,7 +115,7 @@ const PROJECTS: IProject[] = [
     estimatedDurationInMonths: 2,
     tags: ['WinForms', 'Distributed Systems', 'WebSocket'],
     links: [],
-    description: `This desktop application was made in order to simulate behaviour of leader election algorithm (Andrew Tanenbaum's variant) using .NET sockets mechanism and UDP protocol.`,
+    description: `This desktop application demonstrates behaviour of leader election algorithm (Andrew Tanenbaum's variant) using .NET sockets mechanism and UDP protocol.`,
   },
 
   {
@@ -127,7 +127,7 @@ const PROJECTS: IProject[] = [
     estimatedDurationInMonths: 1,
     tags: ['Angular 10', 'RxJS', 'Angular Material'],
     links: [],
-    description: `Short, simple implementation of grocery store API which demonstrates shopping cart, sorting and filtering features from REST API created in Java.`,
+    description: `Short, simple frontend demonstrating shopping cart, sorting and filtering features implemented on server side in Java.`,
   },
 
   {
@@ -156,7 +156,7 @@ const PROJECTS: IProject[] = [
         url: 'https://trolit.github.io/Wordally/',
       },
     ],
-    description: `Tiny tool that offers foreign language vocabulary learning. Approach based on personal experience.`,
+    description: `Tiny tool that for learning foreign language vocabulary.`,
   },
 
   {
@@ -171,7 +171,7 @@ const PROJECTS: IProject[] = [
     links: [],
     youTubeId: 'ZJ2wglKpu5M',
     youTubeVideoInSeparateLine: true,
-    description: `Mobile app that allows to configure set of artists (or playlists) and afterwards on each day - gives random track. Allows to provide e.g. playlists IDs. The motivation behind taking Kotlin was to test different solution after checking out Xamarin.Forms`,
+    description: `Mobile app that promotes listening to music. User configures set of artists (or playlists) and afterwards on each day gets random track according to preferences. The motivation behind taking Kotlin was to test different solution after playing with Xamarin.Forms`,
   },
 
   {
@@ -185,7 +185,7 @@ const PROJECTS: IProject[] = [
     tags: ['Android', 'Xamarin.Forms', 'SQLite'],
     links: [],
     description:
-      'Mobile app designed to provide ability to document any element through photos and then generate PDF file that allows to easily compare changes like durability of hair dye, speed of hair growth, endurance of clothes material etc.',
+      'Mobile app designed to provide ability to document any case through photos and then generate PDF file that allows to easily compare changes like durability of hair dye, speed of hair growth, endurance of clothes material etc.',
   },
 
   {
@@ -198,7 +198,7 @@ const PROJECTS: IProject[] = [
     estimatedDurationInMonths: 1,
     tags: ['WinForms', '.NET Framework'],
     links: [],
-    description: `Small, handy tool to give songs "second life". User provides set of tracks and sShuffler rearranges it's order, based on format that is respected by car audio players.`,
+    description: `Small tool that gives songs "second life" by shuffling their order. It's based on format that is respected by car audio players.`,
   },
 
   {
@@ -233,7 +233,7 @@ const PROJECTS: IProject[] = [
         url: getYouTubeUrl('aM6dEtEPYnU').toString(),
       },
     ],
-    description: 'Small 2D platform game project as second attempt to Unity engine.',
+    description: 'Small 2D platform game project as second attempt to Unity.',
   },
 
   {
@@ -263,8 +263,7 @@ const PROJECTS: IProject[] = [
         url: 'http://przepisownik2016.opx.pl/',
       },
     ],
-    description:
-      'Page for storing culinary recipes. Project was made to play with gridster library and make own web recipe "book".',
+    description: 'Site created to store references to interesting culinary recipes.',
   },
 ];
 
