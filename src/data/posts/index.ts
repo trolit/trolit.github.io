06604ac85df6c96421f3a3702f969fc1730300c4
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-06-03 00:44:51 +0200

    Feature/post about object object mapper implemented in itvault (#129)
    * Create 2024-06-02.ts
    
    * Update index.ts
    
    * Update 2024-06-02.ts
    
    * Update 2024-06-02.ts

--- src/data/posts/index.ts
+++ src/data/posts/index.ts
@@ -11,8 +11,11 @@ import { POST as POST6 } from './2023-12-04';
 import { POST as POST7 } from './2023-12-11';
 import { POST as POST8 } from './2024-05-18';
 import { POST as POST9 } from './2024-05-20';
+import { POST as POST10 } from './2024-06-02';
 
-export const POSTS: IExtendedPost[] = [POST1, POST2, POST3, POST4, POST5, POST6, POST7, POST8, POST9].map((post) => ({
-  ...post,
-  address: translateStringToAddress(post.name, POSTS_ROUTE),
-}));
+export const POSTS: IExtendedPost[] = [POST1, POST2, POST3, POST4, POST5, POST6, POST7, POST8, POST9, POST10].map(
+  (post) => ({
+    ...post,
+    address: translateStringToAddress(post.name, POSTS_ROUTE),
+  }),
+);
